1835a1cb8d6a44ff7f9ebfe851ddb1a48bb58a5e
Tonylac77 <alacournola@gmail.com>
2023-02-02 13:41:29 +0100

    added option for parallel processing
    Former-commit-id: 1479bb60a974ad0bfc5d05173f1527a2b0ae8833
    Former-commit-id: 76d6cb5ab6987799ada9ac883ac8a35d3e572dc4

--- wocondock.py
+++ wocondock.py
@@ -17,15 +17,16 @@ import argparse
 parser = argparse.ArgumentParser(description='Parse required arguments')
 parser.add_argument('--software', required=True, type=str, help ='Path to software folder')
 parser.add_argument('--proteinfile', required=True, type=str, help ='Path to protein file')
-parser.add_argument('--pocket', type = str, choices = ['reference', 'dogsitescorer'], help ='Method to use for pocket determination')
+parser.add_argument('--pocket', required=True, type = str, choices = ['reference', 'dogsitescorer'], help ='Method to use for pocket determination')
 parser.add_argument('--reffile', type=str, help ='Path to reference ligand file')
 parser.add_argument('--dockinglibrary', required=True, type=str, help ='Path to docking library file')
 parser.add_argument('--idcolumn', required=True, type=str, help ='Unique identifier column')
 parser.add_argument('--protonation', required=True, type = str, choices = ['pkasolver', 'GypsumDL', 'None'], help ='Method to use for compound protonation')
-parser.add_argument('--docking', type = str, nargs='+', choices = ['GNINA', 'SMINA', 'PLANTS'], help ='Method(s) to use for docking')
-parser.add_argument('--clustering', type = str, nargs='+', choices = ['RMSD', 'spyRMSD', 'espsim', 'USRCAT', '3DScore', 'bestscore'], help ='Method(s) to use for pose clustering')
+parser.add_argument('--docking', required=True, type = str, nargs='+', choices = ['GNINA', 'SMINA', 'PLANTS'], help ='Method(s) to use for docking')
+parser.add_argument('--clustering', required=True, type = str, nargs='+', choices = ['RMSD', 'spyRMSD', 'espsim', 'USRCAT', '3DScore', 'bestpose'], help ='Method(s) to use for pose clustering')
 parser.add_argument('--nposes', default=10, type=int, help ='Number of poses')
 parser.add_argument('--exhaustiveness', default=8, type = int, help ='Precision of SMINA/GNINA')
+parser.add_argument('--parallel', default=1, type=int, choices = [0,1], help ='Run the workflow in parallel')
 
 args = parser.parse_args()
 
@@ -41,19 +42,32 @@ def run_command(**kwargs):
     elif kwargs.get('pocket') == 'dogsitescorer':
         pocket_definition = binding_site_coordinates_dogsitescorer(kwargs.get('proteinfile'), w_dir, method='volume')
         
-    if Path(w_dir+'/temp/final_library.sdf').isfile() == False:
-        cleaned_df = prepare_library(kwargs.get('dockinglibrary'), kwargs.get('idcolumn'), kwargs.get('software'), kwargs.get('protonation'))
+    if os.path.isfile(w_dir+'/temp/final_library.sdf') == False:
+        prepare_library(kwargs.get('dockinglibrary'), kwargs.get('idcolumn'), kwargs.get('software'), kwargs.get('protonation'))
 
-    if Path(w_dir+'/temp/all_poses.sdf').isfile() == False:
-        all_poses = docking(kwargs.get('proteinfile'), kwargs.get('reffile'), kwargs.get('software'), kwargs.get('docking'), kwargs.get('exhaustiveness'), kwargs.get('nposes'))
+    if kwargs.get('parallel') == 0:
+        docking_func = docking
+        fetch_poses_func = fetch_poses
+        cluster_func = cluster_numpy
+    else:
+        docking_func = docking_splitted
+        fetch_poses_func = fetch_poses_splitted
+        cluster_func = cluster_numpy_futures
+
+    docking_programs = {'GNINA': w_dir+'/temp/gnina/', 'SMINA': w_dir+'/temp/smina/', 'PLANTS': w_dir+'/temp/plants/'}
+    for program, file_path in docking_programs.items():
+        if os.path.isdir(file_path) == False and program in kwargs.get('docking'):
+            docking_func(w_dir, kwargs.get('proteinfile'), kwargs.get('reffile'), kwargs.get('software'), [program], kwargs.get('exhaustiveness'), kwargs.get('nposes'))
+
+    if os.path.isfile(w_dir+'/temp/allposes.sdf') == False:
+        fetch_poses_func(kwargs.get('proteinfile'), kwargs.get('nposes'))
+
+    for method in kwargs.get('clustering'):
+        if os.path.isfile(w_dir+f'/temp/clustering/{method}_clustered.sdf') == False:
+            cluster_func(method, w_dir, kwargs.get('proteinfile'))
     
-    clustering_methods =kwargs.get('clustering').split(' ')
-    for method in clustering_methods:
-        if Path(w_dir+f'/temp/clustering/{method}_clustered.sdf').isfile() == False:
-            cluster(method, w_dir, kwargs.get('proteinfile'))
-    
-    for method in clustering_methods:
-        if Path(w_dir+f'/temp/rescoring_{method}_clustered').ispath() == False:
+    for method in kwargs.get('clustering'):
+        if os.path.isdir(w_dir+f'/temp/rescoring_{method}_clustered') == False:
             rescore_all(w_dir, kwargs.get('proteinfile'), kwargs.get('reffile'), kwargs.get('software'), w_dir+f'/temp/clustering/{method}_clustered.sdf')
 
     apply_ranking_methods_simplified(w_dir)
