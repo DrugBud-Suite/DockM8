6054ca289457190666804ff8e74f385ab13fc30d
Tonylac77 <alacournola@gmail.com>
2023-02-11 09:37:38 +0100

    implemented standardisation and better ECR
    Former-commit-id: ea84511bba5862eb7a3a96cf6568d43e83c0c01c

--- scripts/ranking_functions.py
+++ scripts/ranking_functions.py
@@ -3,33 +3,24 @@ import numpy as np
 import functools
 from scripts.utilities import create_temp_folder
 from sklearn.preprocessing import StandardScaler
+from IPython.display import display
 
-def rank_simplified(dataframe, clustering_method):
-    columns_to_rank = {'GNINA_Affinity':True, 
-                       'GNINA_CNN_Score':False, 
-                       'GNINA_CNN_Affinity':False, 
-                       'Vinardo_Affinity':True, 
-                       'AD4_Affinity':True, 
-                       'RFScoreV1':False, 
-                       'RFScoreV2':False, 
-                       'RFScoreV3':False, 
-                       'PLP':True, 
-                       'CHEMPLP':True, 
-                       'NNScore':False, 
-                       'PLECnn':False}
-    for col,asc in columns_to_rank.items():
-        if col in dataframe.columns:
-            dataframe[f'{col}_RANK_{clustering_method}'] = dataframe[col].rank(method='average', ascending=asc)
-            dataframe = dataframe.drop(col, axis=1)
-    return dataframe
+def standardise_scores(dataframe):
+    columns_to_standardize = ['GNINA_Affinity','Vinardo_Affinity','AD4_Affinity','PLP','CHEMPLP']
+    dataframe[columns_to_standardize] = np.abs(dataframe[columns_to_standardize] - dataframe[columns_to_standardize].max())
+    return dataframe[sorted(dataframe.columns)]
+
+def standardise_rank_scores(dataframe, clustering_method):
+    columns_to_standardize = ['GNINA_Affinity','Vinardo_Affinity','AD4_Affinity','PLP','CHEMPLP']
+    dataframe[columns_to_standardize] = np.abs(dataframe[columns_to_standardize] - dataframe[columns_to_standardize].max())
+    dataframe = dataframe.assign(**{f'{col}_RANK_{clustering_method}': dataframe[col].rank(method='average', ascending=False) for col in dataframe.columns if col != 'Pose ID'})
+    dataframe = dataframe.drop([col for col in dataframe.columns if col != 'Pose ID' and 'RANK' not in col], axis=1)
+    return dataframe[sorted(dataframe.columns)]
 
 def method1_ECR_best(dataframe, clustering_method):
     df = dataframe.copy()
-    #Select columns for calculations
-    dfcolumns = df.columns
-    calc = dfcolumns[1:]
-    #Calculate ECR
-    sigma = 5
+    calc = [col for col in df.columns if col != 'Pose ID']
+    sigma = 0.05 * len(df)
     df = df.apply(lambda x: np.exp(-(x/sigma))/sigma if x.name in calc else x)
     df[f'Method1_ECR_{clustering_method}'] = df.sum(axis=1, numeric_only=True)
     #Return compound ID from Pose ID
@@ -45,9 +36,8 @@ def method1_ECR_best(dataframe, clustering_method):
 def method1_ECR_best_simplified(dataframe, clustering_method):
     df = dataframe.copy()
     #Select columns for calculations
-    calc = df.columns[1:]
-    #Calculate ECR
-    sigma = 5
+    calc = [col for col in df.columns if col != 'Pose ID']
+    sigma = 0.05 * len(df)
     df[calc] = df[calc].apply(lambda x: np.exp(-(x/sigma))/sigma)
     df[f'Method1_ECR_{clustering_method}'] = df[calc].sum(axis=1)
     #Return compound ID from Pose ID
@@ -63,11 +53,8 @@ def method1_ECR_best_simplified(dataframe, clustering_method):
 
 def method2_ECR_average(dataframe, clustering_method):
     df = dataframe.copy()
-    #Select columns for calculations
-    dfcolumns = df.columns
-    calc = dfcolumns[1:]
-    #Calculate ECR
-    sigma = 5
+    calc = [col for col in df.columns if col != 'Pose ID']
+    sigma = 0.05 * len(df)
     df = df.apply(lambda x: np.exp(-(x/sigma))/sigma if x.name in calc else x)
     df[f'Method2_ECR_{clustering_method}'] = df.sum(axis=1, numeric_only=True)
     #Return compound ID from Pose ID
@@ -88,7 +75,7 @@ def method3_avg_ECR(dataframe, clustering_method):
     df = df.groupby('ID', as_index=False).mean(numeric_only=True).round(2)
     df[calc] = df[calc].rank(method='average',ascending=1)
     #Calculate ECR
-    sigma = 5
+    sigma = 0.05 * len(df)
     df[calc] = df[calc].apply(lambda x: np.exp(-(x/sigma))/sigma)
     df[f'Method3_ECR_{clustering_method}'] = df.sum(axis=1, numeric_only=True)
     df = df[['ID', f'Method3_ECR_{clustering_method}']]
@@ -107,8 +94,6 @@ def method4_RbR(dataframe, clustering_method):
 
 def method5_RbV(dataframe, clustering_method):
     df = dataframe.copy()
-    to_flip = ['GNINA_Affinity', 'Vinardo_Affinity', 'AD4_Affinity', 'PLP', 'CHEMPLP']
-    df[to_flip] = -df[to_flip]
     calc = [col for col in df.columns if col != 'Pose ID']
     df['vote'] = 0
     for c in calc:
@@ -161,65 +146,23 @@ def method7_Zscore_avg(dataframe, clustering_method):
     df = df.groupby('ID', as_index=False).mean(numeric_only=True)
     return df
 
-def method8_Zscore_flipped_best(dataframe, clustering_method):
-    df = dataframe.copy()
-    scaler = StandardScaler()
-    #Select columns for calculations and convert to float
-    calc = [col for col in df.columns if col != 'Pose ID']
-    df[calc] = df[calc].apply(pd.to_numeric, errors='coerce')
-    to_flip = ['GNINA_Affinity', 'Vinardo_Affinity', 'AD4_Affinity', 'PLP', 'CHEMPLP']
-    df[to_flip] = df[to_flip]*-1
-    dataframe[calc] = scaler.fit_transform(df[calc])
-    z_scores = (df[calc] - df[calc].mean())/df[calc].std()
-    consensus_scores = z_scores.mean(axis=1)
-    df[f'method8_Zscore_{clustering_method}'] = consensus_scores
-    df = df[['Pose ID', f'method8_Zscore_{clustering_method}', ]]
-    #Return compound ID from Pose ID
-    df[['ID', 'Software', 'Pose Number']] = df['Pose ID'].str.split("_", expand=True)
-    #Drop extra columns
-    df = df.drop(['Software', 'Pose Number'], axis=1)
-    #Aggregate rows using best Avg_Z-score per pose
-    df = df.sort_values(f'method8_Zscore_{clustering_method}', ascending=False).drop_duplicates(['ID'])
-    df.set_index('ID')
-    return df
-
-def method9_Zscore_flipped_avg(dataframe, clustering_method):
-    df = dataframe.copy()
-    scaler = StandardScaler()
-    #Select columns for calculations and convert to float
-    calc = [col for col in df.columns if col != 'Pose ID']
-    df[calc] = df[calc].apply(pd.to_numeric, errors='coerce')
-    to_flip = ['GNINA_Affinity', 'Vinardo_Affinity', 'AD4_Affinity', 'PLP', 'CHEMPLP']
-    df[to_flip] = df[to_flip]*-1
-    df[calc] = scaler.fit_transform(df[calc])
-    z_scores = (df[calc] - df[calc].mean())/df[calc].std()
-    consensus_scores = z_scores.mean(axis=1)
-    df[f'method9_Zscore_{clustering_method}'] = consensus_scores
-    df = df[['Pose ID', f'method9_Zscore_{clustering_method}', ]]
-    #Return compound ID from Pose ID
-    df[['ID', 'Software', 'Pose Number']] = df['Pose ID'].str.split("_", expand=True)
-    #Drop extra columns
-    df = df.drop(['Software', 'Pose Number'], axis=1)
-    #Aggregate rows using best Avg_Z-score per pose
-    df = df.groupby('ID', as_index=False).mean(numeric_only=True)
-    return df
-
-def apply_ranking_methods_simplified(w_dir):
+def apply_ranking_methods_simplified(w_dir, clustering_metrics):
     create_temp_folder(w_dir+'/temp/ranking')
-    ranking_filenames = {'RMSD': 'rescoring_RMSD_clustered', 'espsim': 'rescoring_espsim_clustered', 'spyRMSD': 'rescoring_spyRMSD_clustered', 'USRCAT': 'rescoring_USRCAT_clustered', '3DScore': 'rescoring_3DScore_clustered', 'bestpose': 'rescoring_bestpose_clustered'}
-    rescored_dataframes = {name: pd.read_csv(w_dir+f'/temp/{ranking_filenames[name]}/allposes_rescored.csv') for name in ranking_filenames}
-    for df in rescored_dataframes.values():
-        df.drop(columns=df.columns[0], axis=1, inplace=True)
-    ranked_dataframes = {name+'_ranked': rank_simplified(rescored_dataframes[name], name) for name in ranking_filenames}
-    combined_all_ranks_df = functools.reduce(lambda left, right: pd.merge(left, right, on=['Pose ID'], how='outer'), ranked_dataframes.values())
-    combined_all_ranks_df = combined_all_ranks_df.reindex(columns=['Pose ID'] + [col for col in combined_all_ranks_df.columns if col != 'Pose ID'])
-    combined_all_ranks_df.to_csv(w_dir+'/temp/ranking/ranking_results.csv', index=False)
-    
     rank_methods = {'method1':method1_ECR_best, 'method2':method2_ECR_average, 'method3':method3_avg_ECR, 'method4':method4_RbR}
-    score_methods = {'method5':method5_RbV, 'method6':method6_Zscore_best, 'method7':method7_Zscore_avg, 'method8':method8_Zscore_flipped_best, 'method9':method9_Zscore_flipped_avg}
-    
-    analysed_dataframes = {f'{name}_{method}': rank_methods[method](ranked_dataframes[name+'_ranked'], name) for name in ranking_filenames for method in rank_methods}
-    analysed_dataframes.update({f'{name}_{method}': score_methods[method](rescored_dataframes[name], name) for name in ranking_filenames for method in score_methods})
+    score_methods = {'method5':method5_RbV, 'method6':method6_Zscore_best, 'method7':method7_Zscore_avg}
+    rescoring_folders = {metric:f'rescoring_{metric}_clustered' for metric in clustering_metrics}
+    rescored_dataframes = {name: pd.read_csv(w_dir+f'/temp/{rescoring_folders[name]}/allposes_rescored.csv') for name in rescoring_folders}
+    for df in rescored_dataframes.values():
+        for col in df.columns:
+            if col not in ['Pose ID', 'ID']:
+                df[col] = df[col].apply(pd.to_numeric)
+        df.drop(columns=df.columns[0], axis=1, inplace=True)
+    standardised_dataframes = {name+'_standardised': standardise_scores(rescored_dataframes[name], name) for name in rescoring_folders}
+    ranked_dataframes = {name+'_ranked': standardise_rank_scores(rescored_dataframes[name], name) for name in rescoring_folders}
+    # for name, ranked_df in ranked_dataframes.items():
+    #     ranked_df.to_csv(w_dir+f'/temp/ranking/{name}.csv', index=False)
+    analysed_dataframes = {f'{name}_{method}': rank_methods[method](ranked_dataframes[name+'_ranked'], name) for name in rescoring_folders for method in rank_methods}
+    analysed_dataframes.update({f'{name}_{method}': score_methods[method](standardised_dataframes[name+'_standardised'], name) for name in rescoring_folders for method in score_methods})
     analysed_dataframes = {name: df.drop(columns="Pose ID", errors='ignore') for name, df in analysed_dataframes.items()}
     combined_all_methods_df = functools.reduce(lambda left, right: pd.merge(left, right, on=['ID'], how='outer'), analysed_dataframes.values())
     combined_all_methods_df = combined_all_methods_df.reindex(columns=['ID'] + [col for col in combined_all_methods_df.columns if col != 'ID'])
