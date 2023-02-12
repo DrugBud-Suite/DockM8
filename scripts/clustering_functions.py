432d4243604b2b6736fddf78ec00655db823b19c
Tonylac77 <alacournola@gmail.com>
2023-02-12 23:43:25 +0100

    clean unused functions
    Former-commit-id: 42ca3dfb621127685a814a3e35464f42daacc3f7

--- scripts/clustering_functions.py
+++ scripts/clustering_functions.py
@@ -72,72 +72,6 @@ def affinity_propagation_clustering(input_dataframe):
     return merged_df
 
 def cluster(metric, method, w_dir, protein_file):
-    def matrix_calculation_and_clustering(metric, method, df, id_list, protein_file, w_dir): 
-        clustered_dataframes = []
-        print("*Calculating {} metrics and clustering*".format(metric))
-        metrics = {'RMSD': simpleRMSD_calc, 'spyRMSD': spyRMSD_calc, 'espsim': espsim_calc, 'USRCAT': USRCAT_calc, 'SPLIF': SPLIF_calc, '3DScore': '3DScore', 'bestpose': 'bestpose', 'symmRMSD': symmRMSD_calc}
-        methods = {'Kmedoids': kmedoids_S_clustering, 'AffProp': affinity_propagation_clustering}
-        for id in tqdm(id_list):
-            if metric == 'bestpose':
-                df_name = df[df['ID']==id]
-                df_name[['CHEMPLP', 'SMINA_Affinity', 'CNNaffinity']] = df_name[['CHEMPLP', 'SMINA_Affinity', 'CNNaffinity']].apply(pd.to_numeric, errors='coerce')
-                best_row_CHEMPLP = df_name.loc[df_name.groupby(['ID'])['CHEMPLP'].idxmin()]
-                best_row_SMINA = df_name.loc[df_name.groupby(['ID'])['SMINA_Affinity'].idxmin()]
-                best_row_GNINA = df_name.loc[df_name.groupby(['ID'])['CNNaffinity'].idxmax()]
-                table = pd.concat([best_row_GNINA, best_row_SMINA, best_row_CHEMPLP])
-                table.reset_index(inplace=True)
-                table = pd.DataFrame(table['Pose ID'])
-                table['Pose ID'] = table['Pose ID'].astype(str).str.replace('[()\',]','', regex=False)
-                clustered_dataframes.append(table)
-            elif metric == '3DScore':
-                df_name = df[df['ID']==id]
-                df_name.index = range(len(df_name['Molecule']))
-                table = pd.DataFrame(0.0, index=[df_name['Pose ID']], columns=df_name['Molecule'])
-                for subset in itertools.combinations(df_name['Molecule'], 2):
-                    try:
-                        result = metrics['spyRMSD'](subset[0], subset[1])
-                    except:
-                        result = metrics['RMSD'](subset[0], subset[1])
-                    table.iloc[df_name[df_name['Molecule']==subset[0]].index.values, df_name[df_name['Molecule']==subset[1]].index.values] = 0 if np.isnan(result) else result
-                    table.iloc[df_name[df_name['Molecule']==subset[1]].index.values, df_name[df_name['Molecule']==subset[0]].index.values] = 0 if np.isnan(result) else result
-                table['3DScore'] = table.sum(axis=1)
-                table.sort_values(by='3DScore', ascending=True)
-                table = table.head(1)
-                table.reset_index(inplace=True)
-                table = pd.DataFrame(table['Pose ID'])
-                table['Pose ID'] = table['Pose ID'].astype(str).str.replace('[()\',]','', regex=False)
-                clustered_dataframes.append(table)
-            else:
-                try:
-                    df_name = df[df['ID']==id]
-                    df_name.index = range(len(df_name['Molecule']))
-                    table = pd.DataFrame(0.0, index=[df_name['Pose ID']], columns=df_name['Molecule'])
-                    for subset in itertools.combinations(df_name['Molecule'], 2):
-                        result = metrics[metric](subset[0], subset[1], protein_file)
-                        table.iloc[df_name[df_name['Molecule']==subset[0]].index.values, df_name[df_name['Molecule']==subset[1]].index.values] = 0 if np.isnan(result) else result
-                        table.iloc[df_name[df_name['Molecule']==subset[1]].index.values, df_name[df_name['Molecule']==subset[0]].index.values] = 0 if np.isnan(result) else result
-                    clust_df = methods[method](table)
-                    clust_df=clust_df['Pose ID']
-                    clustered_dataframes.append(clust_df)
-                except:
-                    print(f'Failed to calculate metrics and cluster ID: {id}')
-        full_df = functools.reduce(lambda  left,right: pd.merge(left,right,on=['Pose ID'], how='outer'), clustered_dataframes)
-        full_df['Pose ID'] = full_df['Pose ID'].astype(str).replace('[()\',]','', regex=True)
-        return full_df
-    print('Loading all poses SDF file...')
-    all_poses = PandasTools.LoadSDF(w_dir+'/temp/allposes.sdf', idName='Pose ID', molColName='Molecule', includeFingerprints=False, strictParsing=True)
-    print('Finished loading all poses SDF file...')
-    id_list = np.unique(np.array(all_poses['ID']))
-    create_clustering_folder(w_dir+'/temp/clustering/')
-    clustered_poses = matrix_calculation_and_clustering(metric, method, all_poses, id_list, protein_file, w_dir)
-    clustered_poses = pd.merge(all_poses, clustered_poses, on='Pose ID')
-    # keep only the necessary columns
-    clustered_poses = clustered_poses[['Pose ID', 'Molecule', 'ID']]
-    save_path = w_dir + '/temp/clustering/' + metric + '_clustered.sdf'
-    PandasTools.WriteSDF(clustered_poses, save_path, molColName='Molecule', idName='Pose ID')
-    return
-
-def cluster_numpy(metric, method, w_dir, protein_file):
     def matrix_calculation_and_clustering(metric, method, df, id_list, protein_file): 
         clustered_dataframes = []
         print("*Calculating {} metrics and clustering*".format(metric))
@@ -147,6 +81,21 @@ def cluster_numpy(metric, method, w_dir, protein_file):
             if metric == 'bestpose':
                 df_filtered = df[df['ID']==id]
                 best_pose_output = df_filtered[df_filtered['Pose ID'].str.endswith(('_1', '_01'))]
+                best_pose_output = best_pose_output[['Pose ID']]
+                best_pose_output['Pose ID'] = best_pose_output['Pose ID'].astype(str).str.replace('[()\',]','', regex=False)
+                clustered_dataframes.append(best_pose_output)
+            elif metric == 'bestpose_GNINA':
+                best_pose_output = df[df['Pose ID'].str.endswith('GNINA_1')]
+                best_pose_output = best_pose_output[['Pose ID']]
+                best_pose_output['Pose ID'] = best_pose_output['Pose ID'].astype(str).str.replace('[()\',]','', regex=False)
+                clustered_dataframes.append(best_pose_output)
+            elif metric == 'bestpose_SMINA':
+                best_pose_output = df[df['Pose ID'].str.endswith('SMINA_1')]
+                best_pose_output = best_pose_output[['Pose ID']]
+                best_pose_output['Pose ID'] = best_pose_output['Pose ID'].astype(str).str.replace('[()\',]','', regex=False)
+                clustered_dataframes.append(best_pose_output)
+            elif metric == 'bestpose_PLANTS':
+                best_pose_output = df[df['Pose ID'].str.endswith('PLANTS_01')]
                 best_pose_output = best_pose_output[['Pose ID']]
                 best_pose_output['Pose ID'] = best_pose_output['Pose ID'].astype(str).str.replace('[()\',]','', regex=False)
                 clustered_dataframes.append(best_pose_output)
@@ -202,49 +151,6 @@ def cluster_numpy(metric, method, w_dir, protein_file):
     PandasTools.WriteSDF(clustered_poses, save_path, molColName='Molecule', idName='Pose ID')
     return
 
-def matrix_calculation_and_clustering_futures(metric, method, df, protein_file):
-    metrics = {'RMSD': simpleRMSD_calc, 'spyRMSD': spyRMSD_calc, 'espsim': espsim_calc, 'USRCAT': USRCAT_calc, 'SPLIF': SPLIF_calc, '3DScore': '3DScore', 'bestpose': 'bestpose', 'symmRMSD': symmRMSD_calc}
-    methods = {'Kmedoids': kmedoids_S_clustering, 'AffProp': affinity_propagation_clustering}
-    if metric == 'bestpose':
-        best_pose_output = df[df['Pose ID'].str.endswith(('_1', '_01'))]
-        best_pose_output = best_pose_output[['Pose ID']]
-        best_pose_output['Pose ID'] = best_pose_output['Pose ID'].astype(str).str.replace('[()\',]','', regex=False)
-        return best_pose_output
-    elif metric == '3DScore':
-        subsets = np.array(list(itertools.combinations(df['Molecule'], 2)))
-        indices = {mol: idx for idx, mol in enumerate(df['Molecule'].values)}
-        results = np.zeros(len(subsets))
-        for k, (x, y) in enumerate(subsets):
-            try:
-                results[k] = (metrics['spyRMSD'](x, y, protein_file))
-            except:
-                results[k] = (metrics['RMSD'](x, y, protein_file))
-        i, j = np.array([indices[x] for x in subsets[:,0]]), np.array([indices[y] for y in subsets[:,1]])
-        matrix = np.zeros((len(df), len(df)))
-        matrix[i, j] = results
-        matrix[j, i] = results
-        output_df = pd.DataFrame(matrix, index=df['Pose ID'].values.tolist(), columns=df['Pose ID'].values.tolist())
-        output_df['3DScore'] = output_df.sum(axis=1)
-        output_df.sort_values(by='3DScore', ascending=True, inplace=True)
-        output_df = output_df.head(1)
-        output_df = pd.DataFrame(output_df.index, columns=['Pose ID'])
-        output_df['Pose ID'] = output_df['Pose ID'].astype(str).str.replace('[()\',]','', regex=False)
-        return output_df
-    else:
-        try:
-            subsets = np.array(list(itertools.combinations(df['Molecule'], 2)))
-            indices = {mol: idx for idx, mol in enumerate(df['Molecule'].values)}
-            results = np.array([metrics[metric](x, y, protein_file) for x, y in subsets])
-            i, j = np.array([indices[x] for x in subsets[:,0]]), np.array([indices[y] for y in subsets[:,1]])
-            matrix = np.zeros((len(df), len(df)))
-            matrix[i, j] = results
-            matrix[j, i] = results
-            matrix_df = pd.DataFrame(matrix, index=df['Pose ID'].values.tolist(), columns=df['Pose ID'].values.tolist())
-            clust_df = methods[method](matrix_df)
-            return clust_df
-        except Exception as e:
-            print(f'Failed to calculate metric and cluster : {e}')
-
 def metric_calculation_failure_handling(x, y, metric, protein_file):
     metrics = {'RMSD': simpleRMSD_calc, 'spyRMSD': spyRMSD_calc, 'espsim': espsim_calc, 'USRCAT': USRCAT_calc, 'SPLIF': SPLIF_calc, '3DScore': '3DScore', 'bestpose': 'bestpose', 'symmRMSD': symmRMSD_calc}
     if metric == 'spyRMSD':
@@ -263,6 +169,21 @@ def matrix_calculation_and_clustering_futures_failure_handling(metric, method, d
     methods = {'Kmedoids': kmedoids_S_clustering, 'AffProp': affinity_propagation_clustering}
     if metric == 'bestpose':
         best_pose_output = df[df['Pose ID'].str.endswith(('_1', '_01'))]
+        best_pose_output = best_pose_output[['Pose ID']]
+        best_pose_output['Pose ID'] = best_pose_output['Pose ID'].astype(str).str.replace('[()\',]','', regex=False)
+        return best_pose_output
+    elif metric == 'bestpose_GNINA':
+        best_pose_output = df[df['Pose ID'].str.endswith('GNINA_1')]
+        best_pose_output = best_pose_output[['Pose ID']]
+        best_pose_output['Pose ID'] = best_pose_output['Pose ID'].astype(str).str.replace('[()\',]','', regex=False)
+        return best_pose_output
+    elif metric == 'bestpose_SMINA':
+        best_pose_output = df[df['Pose ID'].str.endswith('SMINA_1')]
+        best_pose_output = best_pose_output[['Pose ID']]
+        best_pose_output['Pose ID'] = best_pose_output['Pose ID'].astype(str).str.replace('[()\',]','', regex=False)
+        return best_pose_output
+    elif metric == 'bestpose_PLANTS':
+        best_pose_output = df[df['Pose ID'].str.endswith('PLANTS_01')]
         best_pose_output = best_pose_output[['Pose ID']]
         best_pose_output['Pose ID'] = best_pose_output['Pose ID'].astype(str).str.replace('[()\',]','', regex=False)
         return best_pose_output
@@ -296,39 +217,7 @@ def matrix_calculation_and_clustering_futures_failure_handling(metric, method, d
         clust_df = methods[method](matrix_df)
         return clust_df
 
-
-def cluster_numpy_futures_failure_handling(metric, method, w_dir, protein_file):
-    print('Loading all poses SDF file...')
-    all_poses = PandasTools.LoadSDF(w_dir+'/temp/allposes.sdf', idName='Pose ID', molColName='Molecule', includeFingerprints=False, strictParsing=True)
-    print('Finished loading all poses SDF file...')
-    id_list = np.unique(np.array(all_poses['ID']))
-    create_clustering_folder(w_dir+'/temp/clustering/')
-    clustered_dataframes = []
-    print(f"*Calculating {metric} metrics and clustering*")
-    with concurrent.futures.ProcessPoolExecutor(max_workers=int(multiprocessing.cpu_count()/2)) as executor:
-        jobs = []
-        for current_id in id_list:
-            try:
-                job = executor.submit(matrix_calculation_and_clustering_futures_failure_handling, metric, method, all_poses[all_poses['ID']==current_id], protein_file)
-                jobs.append(job)
-            except Exception as e:
-                print("Error in concurrent futures job creation: ", str(e))	
-        for job in tqdm(concurrent.futures.as_completed(jobs), total=len(id_list)):
-            try:
-                res = job.result(timeout=60)
-                clustered_dataframes.append(res)
-            except Exception as e:
-                print("Error in concurrent futures job run: ", str(e))
-    display(clustered_dataframes[0])
-    clustered_poses = pd.concat(clustered_dataframes)
-    clustered_poses['Pose ID'] = clustered_poses['Pose ID'].astype(str).replace('[()\',]','', regex=True)
-    clustered_poses = pd.merge(all_poses, clustered_poses, on='Pose ID')
-    clustered_poses = clustered_poses[['Pose ID', 'Molecule', 'ID']]
-    save_path = w_dir + '/temp/clustering/' + metric + '_clustered.sdf'
-    PandasTools.WriteSDF(clustered_poses, save_path, molColName='Molecule', idName='Pose ID')
-    return
-
-def cluster_numpy_futures(metric, method, w_dir, protein_file):
+def cluster_mp(metric, method, w_dir, protein_file):
     print('Loading all poses SDF file...')
     all_poses = PandasTools.LoadSDF(w_dir+'/temp/allposes.sdf', idName='Pose ID', molColName='Molecule', includeFingerprints=False, strictParsing=True)
     print('Finished loading all poses SDF file...')
