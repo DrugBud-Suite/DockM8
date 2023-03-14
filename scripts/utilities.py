bcc16df627753669caa967873701a6c786c9b63d
Tonylac77 <alacournola@gmail.com>
2023-03-14 11:10:26 +0100

    update
    Former-commit-id: 5d0bf126555c01985b5f2469175783fef9b4ff84

--- scripts/utilities.py
+++ scripts/utilities.py
@@ -85,7 +85,7 @@ def printlog(message):
         str(timestamp) + \
         ": "+str(message)
     print(msg)
-    with open('/home/tony/holiday/log.txt', 'a') as f_out:
+    with open('/home/mario/holiday/log.txt', 'a') as f_out:
         f_out.write(msg)
 
 """
