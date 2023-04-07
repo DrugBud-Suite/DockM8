bd0c568f1bbee94d462066aab5306409856b94ce
Tonylac77 <alacournola@gmail.com>
2023-04-07 13:38:14 +0200

    fix folder ceation
    Former-commit-id: 6fc7d0b963ce6bee0b2f43f804b100a690b5550d
    Former-commit-id: 6d40fa2a04fe23d4cacba8fda73306707d8e93f2

--- scripts/performance_calculation.py
+++ scripts/performance_calculation.py
@@ -55,7 +55,7 @@ def process_combination(combination, w_dir, name, standardised_df, ranked_df, co
     selected_columns = list(combination)
     ranked_selected_columns = [column_mapping[col] for col in selected_columns]
     subset_name = '_'.join(selected_columns)
-    replacements_dict = {'_R_': '','_S_': ''}
+    replacements_dict = {'_R_': '','_S_': '_'}
     for key, value in replacements_dict.items():
         subset_name = subset_name.replace(key, value)
     standardised_subset = standardised_df[['ID'] + selected_columns]
@@ -154,7 +154,6 @@ def calculate_EF_single_functions(w_dir, docking_library, clustering_metrics):
             merged_df = pd.merge(std_df_grouped, original_df, on='ID')
             for col in merged_df.columns:
                 if col not in ['ID', 'Activity']:
-                    print(col)
                     sorted_df = merged_df.sort_values(col, ascending = False)
                     N10_percent = round(0.10 * len(sorted_df))
                     N1_percent = round(0.01 * len(sorted_df))
@@ -170,4 +169,5 @@ def calculate_EF_single_functions(w_dir, docking_library, clustering_metrics):
                         'EF10%': ef10,
                         'EF1%': ef1
                     }, ignore_index=True)
+    create_temp_folder(w_dir+'/temp/consensus')
     EF_results.to_csv(w_dir+'/temp/consensus/EF_single_functions.csv')
