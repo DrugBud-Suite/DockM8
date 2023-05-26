b289ff52f5337f3471f6d33dab0fadcd27d262b9
hamzaibrahim21 <hamzaibrahiem@gmail.com>
2023-05-26 15:34:18 +0200

    fix env name

--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ mamba env create --file $PROJECT_DIR/environment.yml --force
 
 # activate the conda env before installing PyTorch Geometric via pip
 conda init bash
-source activate dockm82
+source activate dockm8
 
 #Specify your CUDA toolkit
 CUDA=cpu
@@ -23,9 +23,7 @@ python -m pip install torch-scatter==2.1.0 -f https://data.pyg.org/whl/torch-1.9
 python -m pip install torch-sparse==0.6.12 -f https://data.pyg.org/whl/torch-1.9.1+${CUDA}.html
 python -m pip install torch-spline-conv==1.2.1 -f https://data.pyg.org/whl/torch-1.9.1+${CUDA}.html
 python -m pip install torch-cluster==1.6.0 -f https://data.pyg.org/whl/torch-1.9.1+${CUDA}.html
-#pip install torch-geometric==2.3.1 -f https://data.pyg.org/whl/torch-1.9.1+${CUDA}.html
-#python -m pip install torch-geometric
-#python -m pip install torch_scatter torch_sparse torch_cluster torch_spline_conv  -f https://data.pyg.org/whl/torch-1.9.1+${CUDA}.html
+
 
 
 #python -m pip install torch-scatter==2.1.0 torch-sparse==0.6.12 torch-spline-conv==1.2.1 torch-cluster==1.6.0 torch-geometric==2.0.1 -f https://data.pyg.org/whl/torch-1.9.1+cpu.html
