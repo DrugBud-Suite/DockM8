a26e5c372a429d0b06e1a6c8a5aa17af9ba66fd9
Tonylac77 <alacournola@gmail.com>
2023-06-05 16:36:38 +0200

    added pdbqt conversion with Meeko
    Former-commit-id: 5d0ab79bd24f283c5dd2cb357978b0c410ef2e58

--- scripts/utilities.py
+++ scripts/utilities.py
@@ -11,6 +11,7 @@ def create_temp_folder(path, silent=False):
         
 from rdkit.Chem import PandasTools
 from tqdm import tqdm
+import math
         
 def split_sdf(dir, sdf_file, ncpus):
     sdf_file_name = os.path.basename(sdf_file).replace('.sdf', '')
@@ -20,7 +21,7 @@ def split_sdf(dir, sdf_file, ncpus):
     for file in os.listdir(dir+f'/split_{sdf_file_name}'):
         os.unlink(os.path.join(dir+f'/split_{sdf_file_name}', file))
     df = PandasTools.LoadSDF(sdf_file, molColName='Molecule', idName='ID', includeFingerprints=False, strictParsing=True)
-    compounds_per_core = round(len(df['ID'])/(ncpus*2))
+    compounds_per_core = math.ceil(len(df['ID'])/(ncpus*2))
     used_ids = set() # keep track of used 'ID' values
     file_counter = 1
     for i in tqdm(range(0, len(df), compounds_per_core), desc='Splitting files'):
@@ -32,6 +33,7 @@ def split_sdf(dir, sdf_file, ncpus):
         file_counter+=1
     print(f'Split docking library into {file_counter-1} files each containing {compounds_per_core} compounds')
     return split_files_folder
+
 
 def split_sdf_single(dir, sdf_file):
     sdf_file_name = os.path.basename(sdf_file).replace('.sdf', '')
@@ -145,3 +147,76 @@ def parallel_sdf_to_pdbqt(input_file, output_dir, ncpus):
     except Exception as e:
         print(f"ERROR: Could note convert SDF file to .pdbqt: {e}")
     return len(molecules)
+
+from meeko import MoleculePreparation
+
+def meeko_to_pdbqt(sdf_path, output_dir):
+    for mol in Chem.SDMolSupplier(sdf_path, removeHs=False):
+        preparator = MoleculePreparation(min_ring_size=10)
+        mol = Chem.AddHs(mol)
+        preparator.prepare(mol)
+        pdbqt_string = preparator.write_pdbqt_string()
+        
+        # Extract the molecule name from the SDF file
+        mol_name = mol.GetProp('_Name')
+
+        # Create the output file path
+        output_path = os.path.join(output_dir, f"{mol_name}.pdbqt")
+
+        # Write the pdbqt string to the file
+        with open(output_path, 'w') as f:
+            f.write(pdbqt_string)
+    
+from rdkit import Chem
+from rdkit.Chem import AllChem
+    
+def load_molecule(molecule_file):
+    """Load a molecule from a file.
+    Parameters
+    ----------
+    molecule_file : str
+        Path to file for storing a molecule, which can be of format '.mol2', '.mol', '.sdf',
+        '.pdbqt', or '.pdb'.
+    Returns
+    -------
+    mol : rdkit.Chem.rdchem.Mol
+        RDKit molecule instance for the loaded molecule.
+    """
+    if molecule_file.endswith('.mol2'):
+        mol = Chem.MolFromMol2File(molecule_file, sanitize=False, removeHs=False)
+    if molecule_file.endswith('.mol'):
+        mol = Chem.MolFromMolFile(molecule_file, sanitize=False, removeHs=False)
+    elif molecule_file.endswith('.sdf'):
+        supplier = Chem.SDMolSupplier(molecule_file, sanitize=False, removeHs=False)
+        mol = supplier[0]
+    elif molecule_file.endswith('.pdbqt'):
+        with open(molecule_file) as f:
+            pdbqt_data = f.readlines()
+        pdb_block = ''
+        for line in pdbqt_data:
+            pdb_block += '{}\n'.format(line[:66])
+        mol = Chem.MolFromPDBBlock(pdb_block, sanitize=False, removeHs=False)
+    elif molecule_file.endswith('.pdb'):
+        mol = Chem.MolFromPDBFile(molecule_file, sanitize=False, removeHs=False)
+    else:
+        return ValueError(f'Expect the format of the molecule_file to be '
+                          'one of .mol2, .mol, .sdf, .pdbqt and .pdb, got {molecule_file}')
+    return mol
+
+import subprocess
+from subprocess import DEVNULL, STDOUT
+
+def convert_pdb_to_pdbqt(protein_file):
+    # Define output file name
+    pdbqt_file = protein_file.replace('.pdb', '.pdbqt')
+
+    # Open Babel command
+    obabel_command = f'obabel {protein_file} -O {pdbqt_file} -partialcharges Gasteiger -xr'
+
+    # Execute command
+    try:
+        subprocess.call(obabel_command, shell=True, stdout=DEVNULL, stderr=STDOUT)
+    except Exception as e:
+        print(f'Conversion from PDB to PDBQT failed: {e}')
+
+    return pdbqt_file
