f14576e75113bdb3278f869e2dffacd26b86a4e4
Tonylac77 <alacournola@gmail.com>
2023-09-06 17:39:09 +0000

    refactoring

--- scripts/clustering_functions.py
+++ scripts/clustering_functions.py
@@ -21,45 +21,62 @@ import time
 from pathlib import Path
 
 
-def kmedoids_S_clustering(input_dataframe):
-    '''This function applies kmedoids clustering to the input dataframe, which is a matrix of clustering metrics. It returns
-    the list of cluster centers and their Pose ID'''
+def kmedoids_S_clustering(input_dataframe: pd.DataFrame) -> pd.DataFrame:
+    """
+    Applies k-medoids clustering to the input dataframe, which contains clustering metrics.
+    Calculates the silhouette scores for different numbers of clusters and selects the optimal number of clusters
+    based on the highest silhouette score. Then, it performs k-medoids clustering with the optimal number of clusters
+    and returns the list of cluster centers and their corresponding Pose IDs.
+
+    Args:
+        input_dataframe: A dataframe containing clustering metrics. The columns represent different molecules
+        and the rows represent different poses.
+
+    Returns:
+        A dataframe containing the Pose IDs of the cluster centers.
+    """
     df = input_dataframe.copy()
-    molecule_list = input_dataframe.columns.values.tolist()
-    # preprocessing our data *scaling data*
+    molecule_list = input_dataframe.columns.tolist()
+
+    # Scale the values of the molecules in the dataframe using StandardScaler
     scaler = StandardScaler()
     df[molecule_list] = scaler.fit_transform(df)
+
     silhouette_scores = {}
     for num_clusters in range(2, 5):
-        # calculating silhouette average score for every cluster and plotting them at the end
-        # choosing pam method as it's more accurate
-        # initialization of medoids is a greedy approach, as it's more
-        # effecient as well
+        # Calculate silhouette average score for every cluster and select the optimal number of clusters
+        # Choosing PAM method as it's more accurate
         kmedoids = KMedoids(
             n_clusters=num_clusters,
             method='pam',
             init='build',
-            max_iter=150)
+            max_iter=150
+        )
         kmedoids.fit_predict(df)
         silhouette_average_score = silhouette_score(df, kmedoids.labels_)
         silhouette_scores[num_clusters] = silhouette_average_score
+
     optimum_no_clusters = max(silhouette_scores, key=silhouette_scores.get)
-    # # Apply optimised k-medoids clustering
+
+    # Apply optimized k-medoids clustering
     kmedoids = KMedoids(
         n_clusters=optimum_no_clusters,
         method='pam',
         init='build',
-        max_iter=150)
+        max_iter=150
+    )
     clusters = kmedoids.fit_predict(df)
     df['KMedoids Cluster'] = clusters
     df['Pose ID'] = molecule_list
-    # Determine centers
+
+    # Determine cluster centers
     centroids = kmedoids.cluster_centers_
     cluster_centers = pd.DataFrame(centroids, columns=molecule_list)
-    # rearranging data
+
+    # Merge the dataframe with the cluster labels and the dataframe of cluster centers on the molecule list
     merged_df = pd.merge(df, cluster_centers, on=molecule_list, how='inner')
     merged_df = merged_df[['Pose ID']]
-    # .astype(str).replace('[()\',]','', regex=False)
+
     return merged_df
 
 
