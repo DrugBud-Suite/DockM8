ab06bfcfb7e1811fca14086a9c5a2fbfb9c8e3da
Tonylac77 <alacournola@gmail.com>
2023-09-07 06:33:16 +0000

    refactor, add docstrings

--- scripts/library_preparation.py
+++ scripts/library_preparation.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import os
 import subprocess
 from subprocess import DEVNULL, STDOUT, PIPE
@@ -20,6 +21,21 @@ def standardize_molecule(molecule):
 
 
 def standardize_library(input_sdf, id_column):
+    """
+    Standardizes a docking library using the ChemBL Structure Pipeline.
+
+    Args:
+        input_sdf (str): The path to the input SDF file containing the docking library.
+        id_column (str): The name of the column in the SDF file that contains the compound IDs.
+
+    Returns:
+        None. The function writes the standardized molecules to a new SDF file.
+
+    Raises:
+        Exception: If there is an error loading the library SDF file.
+        Exception: If there is an error converting SMILES to RDKit molecules.
+        Exception: If there is an error writing the standardized library SDF file.
+    """
     printlog('Standardizing docking library using ChemBL Structure Pipeline...')
     # Load Original Library SDF into Pandas
     try:
@@ -36,19 +52,15 @@ def standardize_library(input_sdf, id_column):
         n_cpds_start = len(df)
     except BaseException:
         printlog('ERROR: Failed to Load library SDF file!')
+        raise Exception('Failed to Load library SDF file!')
     try:
         df.drop(columns='Molecule', inplace=True)
-        df['Molecule'] = [Chem.MolFromSmiles(
-            smiles) for smiles in df['SMILES']]
-    except BaseException:
-        printlog('ERROR: Failed to convert SMILES to RDKit molecules!')
+        df['Molecule'] = [Chem.MolFromSmiles(smiles) for smiles in df['SMILES']]
+    except Exception as e:
+        printlog('ERROR: Failed to convert SMILES to RDKit molecules!' + e)
     # Standardize molecules using ChemBL Pipeline
-    df['Molecule'] = [standardizer.standardize_mol(
-        mol) for mol in df['Molecule']]
-    df['Molecule'] = [standardizer.get_parent_mol(
-        mol) for mol in df['Molecule']]
-    df[['Molecule', 'flag']] = pd.DataFrame(
-        df['Molecule'].tolist(), index=df.index)
+    df['Molecule'] = [standardizer.get_parent_mol(standardizer.standardize_mol(mol)) for mol in df['Molecule']]
+    df[['Molecule', 'flag']] = pd.DataFrame(df['Molecule'].tolist(), index=df.index)
     df = df.drop(columns='flag')
     df = df.loc[:, ~df.columns.duplicated()].copy()
     n_cpds_end = len(df)
@@ -66,10 +78,22 @@ def standardize_library(input_sdf, id_column):
             allNumeric=True)
     except BaseException:
         printlog('ERROR: Failed to write standardized library SDF file!')
-    return
+        raise Exception('Failed to write standardized library SDF file!')
 
 
 def standardize_library_futures(input_sdf, id_column, ncpus):
+    """
+    Standardizes a docking library using the ChemBL Structure Pipeline.
+
+    Args:
+        input_sdf (str): The path to the input SDF file containing the docking library.
+        id_column (str): The name of the column in the SDF file that contains the compound IDs.
+        ncpus (int): The number of CPUs to use for parallelization.
+
+    Returns:
+        pandas.DataFrame: The standardized DataFrame containing the standardized molecules.
+    """
+
     printlog('Standardizing docking library using ChemBL Structure Pipeline...')
     try:
         df = PandasTools.LoadSDF(
@@ -111,7 +135,7 @@ def standardize_library_futures(input_sdf, id_column, ncpus):
         str(output_sdf),
         molColName='Molecule',
         idName='ID')
-    return
+    return df
 
 
 def protonate_library_pkasolver(input_sdf):
@@ -207,52 +231,77 @@ def generate_conformers_GypsumDL_noprotonation(
         printlog(e)
 
 
-def cleanup(input_sdf):
+def cleanup(input_sdf: str) -> pd.DataFrame:
+    """
+    Cleans up the temporary files generated during the library preparation process.
+
+    Args:
+        input_sdf (str): The path to the input SDF file containing the compound library.
+
+    Returns:
+        pd.DataFrame: The final DataFrame containing the cleaned-up library with only the 'Molecule' and 'ID' columns.
+    """
     printlog('Cleaning up files...')
     wdir = Path(input_sdf).parent
-    gypsum_df = PandasTools.LoadSDF(
-        str(
-            wdir /
-            'temp' /
-            'gypsum_dl_success.sdf'),
-        idName='ID',
-        molColName='Molecule',
-        strictParsing=True)
+
+    # Load the successfully generated conformers from the GypsumDL process into a pandas DataFrame
+    gypsum_df = pd.read_sdf(wdir / 'temp' / 'gypsum_dl_success.sdf')
+
+    # Remove the first row of the DataFrame, which contains the original input molecule
     final_df = gypsum_df.iloc[1:, :]
+
+    # Select only the 'Molecule' and 'ID' columns from the DataFrame
     final_df = final_df[['Molecule', 'ID']]
+
+    # Get the number of compounds in the final DataFrame
     n_cpds_end = len(final_df)
-    printlog(
-        f'Preparation of compound library finished: ended with {n_cpds_end}')
-    PandasTools.WriteSDF(final_df,
-                         str(wdir / 'temp' / 'final_library.sdf'),
-                         molColName='Molecule',
-                         idName='ID')
+
+    # Write the final DataFrame to a new SDF file
+    final_df.to_sdf(wdir / 'temp' / 'final_library.sdf')
+
+    # Delete the temporary files generated during the library preparation process
     (wdir / 'temp' / 'gypsum_dl_success.sdf').unlink(missing_ok=True)
     (wdir / 'temp' / 'protonated_library.sdf').unlink(missing_ok=True)
     (wdir / 'temp' / 'standardized_library.sdf').unlink(missing_ok=True)
     (wdir / 'temp' / 'gypsum_dl_failed.smi').unlink(missing_ok=True)
+
+    printlog(f'Preparation of compound library finished: ended with {n_cpds_end}')
+
     return final_df
 
 
-def prepare_library(input_sdf, id_column, protonation, ncpus):
+def prepare_library(input_sdf: str, id_column: str, protonation: str, ncpus: int) -> pd.DataFrame:
+    """
+    Prepares a docking library for further analysis.
+    
+    Args:
+        input_sdf (str): The path to the input SDF file containing the docking library.
+        id_column (str): The name of the column in the SDF file that contains the compound IDs.
+        protonation (str): The method to use for protonation. Can be 'pkasolver', 'GypsumDL', or any other value for no protonation.
+        ncpus (int): The number of CPUs to use for parallelization.
+        
+    Returns:
+        pd.DataFrame: The final cleaned DataFrame containing the standardized, protonated (if applicable), and 3D conformer-generated molecules.
+    """
     wdir = Path(input_sdf).parent
     standardized_sdf = wdir / 'temp' / 'standardized_library.sdf'
+    
     if not standardized_sdf.is_file():
         standardize_library_futures(input_sdf, id_column, ncpus)
+    
     protonated_sdf = wdir / 'temp' / 'protonated_library.sdf'
+    
     if not protonated_sdf.is_file():
         if protonation == 'pkasolver':
             protonate_library_pkasolver(standardized_sdf)
-            generate_conformers_GypsumDL_noprotonation(
-                protonated_sdf,  ncpus)
+            generate_conformers_GypsumDL_noprotonation(protonated_sdf, ncpus)
         elif protonation == 'GypsumDL':
-            generate_conformers_GypsumDL_withprotonation(
-                standardized_sdf,  ncpus)
+            generate_conformers_GypsumDL_withprotonation(standardized_sdf, ncpus)
         else:
-            generate_conformers_GypsumDL_noprotonation(
-                standardized_sdf,  ncpus)
+            generate_conformers_GypsumDL_noprotonation(standardized_sdf, ncpus)
     else:
-        generate_conformers_GypsumDL_noprotonation(
-            protonated_sdf, ncpus)
+        generate_conformers_GypsumDL_noprotonation(protonated_sdf, ncpus)
+    
     cleaned_df = cleanup(input_sdf)
     return cleaned_df
+
