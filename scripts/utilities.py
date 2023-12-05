c66c24857329f762004600fc738de218fd0bd87e
Tonylac77 <alacournola@gmail.com>
2023-12-05 10:43:55 +0100

    improve parsing str2bool

--- scripts/utilities.py
+++ scripts/utilities.py
@@ -326,9 +326,9 @@ def str2bool(v):
     """
     if isinstance(v, bool):
         return v
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+    if v.lower() in ('yes', 'true', 't', 'y', '1', 'True'):
         return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+    elif v.lower() in ('no', 'false', 'f', 'n', '0', 'False'):
         return False
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
