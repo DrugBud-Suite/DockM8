cf6164b18ef58194172ebca8fd71c4a5a627aa63
Tonylac77 <alacournola@gmail.com>
2024-05-02 16:56:07 +0200

    Refactor test_pocket_finder.py to use pytest fixtures and organize imports

--- tests/test_pocket_finder.py
+++ tests/test_pocket_finder.py
@@ -2,12 +2,12 @@ import pytest
 from pathlib import Path
 import sys
 import os
-from scripts.pocket_finding.main import pocket_finder
 import warnings
 
 # Ensure the scripts directory is accessible
 sys.path.append(os.path.abspath('..'))
 
+from scripts.pocket_finding.main import pocket_finder
 
 # Ignore deprecation warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -18,12 +18,13 @@ def common_test_data():
     base_path = Path(__file__).parent.resolve()
     ligand = base_path / 'test_files/1fvv_l.sdf'
     receptor = base_path / 'test_files/1fvv_p.pdb'
+    w_dir = base_path / 'test_files/1fvv_p_protoss'
     radius = 10
-    return ligand, receptor, radius
+    return ligand, receptor, w_dir, radius
 
 def test_reference_mode(common_test_data):
     """Test pocket finding in reference mode."""
-    ligand, receptor, radius = common_test_data
+    ligand, receptor, w_dir, radius = common_test_data
     pocket_definition = pocket_finder('Reference', receptor=receptor, ligand=ligand, radius=radius)
     print(pocket_definition)
     expected_output = {'center': [-9.67, 207.73, 113.41], 'size': [20.0, 20.0, 20.0]}
@@ -32,17 +33,17 @@ def test_reference_mode(common_test_data):
 
 def test_rog_mode(common_test_data):
     """Test pocket finding in radius of gyration (RoG) mode."""
-    ligand, receptor, _ = common_test_data
+    ligand, receptor, w_dir, radius = common_test_data
     pocket_definition = pocket_finder('RoG', receptor=receptor, ligand=ligand)
     print(pocket_definition)
     expected_output = {'center': [-9.67, 207.73, 113.41], 'size': [14.73, 14.73, 14.73]}
     assert pocket_definition == expected_output
 
-def test_dogsitescorer_mode():
+def test_dogsitescorer_mode(common_test_data):
     """Test pocket finding using Dogsitescorer."""
-    w_dir = Path('dockm8_testing/1fvv_p_protoss')
-    receptor = Path('test_files/1fvv_p.pdb')
-    pocket_definition = pocket_finder('Dogsitescorer', receptor=receptor, w_dir=w_dir, method='volume') 
+    ligand, receptor, w_dir, radius = common_test_data
+    w_dir = Path(__file__).parent.resolve()
+    pocket_definition = pocket_finder('Dogsitescorer', receptor=receptor, w_dir=w_dir) 
     expected_output = {'center': [206.57, 113.81, 17.46], 'size': [17.46, 17.46, 17.46]}
     assert pocket_definition == expected_output
 
