c323958508c7bfd222dfab76a5921a415754cbce
Tonylac77 <alacournola@gmail.com>
2024-05-03 17:17:57 +0200

    add tests for new library preparation scripts

---
+++ tests/library_preparation/conformer_generation/test_confomer_generation.py
@@ -0,0 +1,77 @@
+import os
+import sys
+from pathlib import Path
+import pytest
+from rdkit.Chem import PandasTools
+
+cwd = Path.cwd()
+dockm8_path = cwd.parents[0] / "DockM8"
+sys.path.append(str(dockm8_path))
+
+from scripts.library_preparation.conformer_generation.confgen_GypsumDL import (
+    generate_conformers_GypsumDL,
+)
+from scripts.library_preparation.conformer_generation.confgen_RDKit import (
+    generate_conformers_RDKit,
+)
+
+@pytest.fixture
+def common_test_data():
+    """Set up common test data."""
+    library = dockm8_path / "tests/test_files/library.sdf"
+    output_dir = dockm8_path / "tests/test_files/"
+    software = dockm8_path / "software"
+    return library, output_dir, software
+
+
+def test_generate_conformers_GypsumDL(common_test_data):
+    """Test generate_conformers_GypsumDL function."""
+    library, output_dir, software = common_test_data
+    ncpus = int(os.cpu_count() * 0.9)
+
+    library_df = PandasTools.LoadSDF(str(library), molColName=None, idName="ID")
+
+    # Call the function
+    output_file = generate_conformers_GypsumDL(library, output_dir, software, ncpus)
+
+    output_df = PandasTools.LoadSDF(str(output_file), molColName=None, idName="ID")
+
+    # Check if the output file exists
+    assert output_file.exists()
+
+    # Check if the output file is in the correct directory
+    assert output_file.name == "generated_conformers.sdf"
+
+    # Check if the number of molecules in the input and output files are the same
+    assert len(library_df) == len(output_df)
+
+    # Check if the unnecessary files are correctly removed
+    assert not (output_dir / "GypsumDL_results").exists()
+    assert not (output_dir / "GypsumDL_split").exists()
+    assert not (output_dir / "gypsum_dl_success.sdf").exists()
+    assert not (output_dir / "gypsum_dl_failed.smi").exists()
+
+
+def test_generate_conformers_RDKit(common_test_data):
+    """Test generate_conformers_RDKit function."""
+    library, output_dir, software = common_test_data
+    ncpus = int(os.cpu_count() * 0.9)
+
+    library_df = PandasTools.LoadSDF(str(library), molColName=None, idName="ID")
+
+    # Call the function
+    output_file = generate_conformers_RDKit(library, output_dir, ncpus)
+
+    output_df = PandasTools.LoadSDF(str(output_file), molColName=None, idName="ID")
+
+    # Check if the output file exists
+    assert output_file.exists()
+
+    # Check if the output file is in the correct directory
+    assert output_file.parent == output_dir
+
+    # Check if the output file has the correct name
+    assert output_file.name == "generated_conformers.sdf"
+
+    # Check if the number of molecules in the input and output files are the same
+    assert len(library_df) == len(output_df)
---
+++ tests/library_preparation/standardisation/test_standardisation.py
@@ -0,0 +1,48 @@
+import os
+import sys
+from pathlib import Path
+
+import pytest
+from rdkit.Chem import PandasTools
+
+cwd = Path.cwd()
+dockm8_path = cwd.parents[0] / "DockM8"
+sys.path.append(str(dockm8_path))
+
+from scripts.library_preparation.standardisation.standardise import standardize_library
+
+@pytest.fixture
+def common_test_data():
+    """Set up common test data."""
+    library = dockm8_path / "tests/test_files/library.sdf"
+    output_dir = dockm8_path / "tests/test_files/"
+    id_column = "ID"
+    return library, output_dir, id_column
+
+def test_standardize_library(common_test_data):
+    # Define the input parameters for the function
+    library, output_dir, id_column = common_test_data
+    ncpus = int(os.cpu_count() * 0.9)
+    # Call the function
+    standardized_file = standardize_library(library, output_dir, id_column, ncpus)
+    # Add your assertions here to verify the expected behavior of the function
+
+    # Verify that the standardized library file exists
+    assert (output_dir / "standardized_library.sdf").exists()
+
+    # Verify that the standardized library file is not empty
+    assert (output_dir / "standardized_library.sdf").stat().st_size > 0
+
+    # Verify that the standardized library file is a valid SDF file
+    try:
+        PandasTools.LoadSDF(str(output_dir / "standardized_library.sdf"))
+    except Exception as e:
+        assert False, f"Failed to load standardized library SDF file: {str(e)}"
+
+    # Verify that the number of compounds in the standardized library is the same as the input library
+    input_df = PandasTools.LoadSDF(str(library), molColName=None, idName=id_column)
+    standardized_df = PandasTools.LoadSDF(str(standardized_file), molColName=None, idName=id_column)
+    assert len(input_df) == len(standardized_df)
+
+    # Verify that the standardized library does not contain any duplicate compounds
+    assert len(standardized_df.drop_duplicates(subset="ID")) == len(standardized_df)
