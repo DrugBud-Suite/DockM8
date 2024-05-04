a653c13c7f947e725126b990a754ff4fb2240060
Tonylac77 <alacournola@gmail.com>
2024-05-04 08:18:45 +0200

    improve DockM8 path finding logic

--- dockm8.py
+++ dockm8.py
@@ -10,7 +10,7 @@ import time
 from rdkit.Chem import PandasTools
 
 cwd = Path.cwd()
-dockm8_path = cwd.parents[0] / "DockM8"
+dockm8_path = next((path for path in cwd.parents if path.name == "DockM8"), None)
 sys.path.append(str(dockm8_path))
 
 # Import modules for docking, scoring, protein and ligand preparation, etc.
