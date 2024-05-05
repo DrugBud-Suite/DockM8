6fe7fc6c4edbd186714c172c74151c00e37e3803
Tonylac77 <alacournola@gmail.com>
2024-05-05 17:46:14 +0200

    refactor: Improve code readability and remove unnecessary files in protein preparation script

--- tests/protein_preparation/structure_assessment/test_edia.py
+++ tests/protein_preparation/structure_assessment/test_edia.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from pathlib import Path
 
@@ -5,7 +6,10 @@ import pytest
 from Bio.PDB import PDBParser
 
 # Search for 'DockM8' in parent directories
-dockm8_path = next((p / 'DockM8' for p in Path(__file__).resolve().parents if (p / 'DockM8').is_dir()), None)
+dockm8_path = next(
+    (p / "DockM8" for p in Path(__file__).resolve().parents if (p / "DockM8").is_dir()),
+    None,
+)
 sys.path.append(str(dockm8_path))
 
 from scripts.protein_preparation.structure_assessment.edia import get_best_chain_edia
@@ -15,22 +19,24 @@ from scripts.protein_preparation.structure_assessment.edia import get_best_chain
 def common_test_data():
     """Set up common test data."""
     pdb_code = "2o1x"
-    output_dir = dockm8_path / 'tests/test_files'
+    output_dir = dockm8_path / "tests/test_files"
     return pdb_code, output_dir
 
 
 def test_get_best_chain_edia(common_test_data):
     """Test getting the best chain using EDIA."""
     pdb_code, output_dir = common_test_data
-    extracted_chain_path = get_best_chain_edia(pdb_code, output_dir)
+    output_path = get_best_chain_edia(pdb_code, output_dir)
     expected_chain_path = output_dir / f"{pdb_code}_A.pdb"
-    assert extracted_chain_path.is_file()
-    assert extracted_chain_path == expected_chain_path
+    assert output_path.is_file()
+    assert output_path == expected_chain_path
     # Verify that the extracted chain file is not empty
-    assert extracted_chain_path.stat().st_size > 0
+    assert output_path.stat().st_size > 0
     parser = PDBParser()
-    structure = parser.get_structure('structure', extracted_chain_path)
+    structure = parser.get_structure("structure", output_path)
     assert structure is not None
+    os.unlink(output_path) if os.path.exists(output_path) else None
+
 
 def test_get_best_chain_edia_invalid_pdb_code(common_test_data):
     """Test getting the best chain using EDIA with an invalid PDB code."""
