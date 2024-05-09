cb902359d75715254f577e674e55dfa391409d1a
Tonylac77 <alacournola@gmail.com>
2024-05-09 22:50:36 +0200

    refactor: Update ncpus to n_cpus

--- scripts/library_preparation/protonation/protgen_GypsumDL.py
+++ scripts/library_preparation/protonation/protgen_GypsumDL.py
@@ -21,7 +21,7 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
 def protonate_GypsumDL(
-    input_sdf: Path, output_dir: Path, software: Path, ncpus: int
+    input_sdf: Path, output_dir: Path, software: Path, n_cpus: int
 ):
     """
     Generates protonation states using GypsumDL.
@@ -30,7 +30,7 @@ def protonate_GypsumDL(
         input_sdf (str): Path to the input SDF file.
         output_dir (str): Path to the output directory.
         software (str): Path to the GypsumDL software.
-        ncpus (int): Number of CPUs to use for the calculation.
+        n_cpus (int): Number of CPUs to use for the calculation.
 
     Raises:
         Exception: If failed to generate protomers.
@@ -85,7 +85,7 @@ def protonate_GypsumDL(
         split_files_sdfs,
         3,
         output_dir=output_dir,
-        cpus=math.ceil(ncpus // 3),
+        cpus=math.ceil(n_cpus // 3),
     )
 
     results_dfs = []
@@ -96,7 +96,7 @@ def protonate_GypsumDL(
                 str(output_dir / "GypsumDL_results" / file),
                 molColName="Molecule",
                 idName="ID",
-                removeHs=False,
+                
             )
             results_dfs.append(sdf_df)
 
