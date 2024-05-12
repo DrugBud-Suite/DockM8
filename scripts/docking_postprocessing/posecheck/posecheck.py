9bbcff17dc11112f61a5c7cb75685dbbb1c40960
Tonylac77 <alacournola@gmail.com>
2024-05-12 15:10:16 +0200

    chore: Add parallelised posecheck script for post-processing docking results

---
+++ scripts/docking_postprocessing/posecheck/posecheck.py
@@ -0,0 +1,71 @@
+import os
+import sys
+import time
+from pathlib import Path
+
+import pandas as pd
+from posecheck import PoseCheck
+
+# Search for 'DockM8' in parent directories
+dockm8_path = next(
+    (p / "DockM8" for p in Path(__file__).resolve().parents if (p / "DockM8").is_dir()),
+    None,
+)
+sys.path.append(str(dockm8_path))
+
+import multiprocessing
+
+from scripts.utilities.utilities import printlog
+
+
+def pose_checker(
+    dataframe: pd.DataFrame,
+    protein_file: Path,
+    clash_cutoff: int = 5,
+    strain_cutoff: int = 5000,
+    n_cpus: int = int(os.cpu_count() * 0.9),
+):
+    try:
+        tic = time.perf_counter()
+        printlog("Checking poses using PoseCheck...")
+        pc = PoseCheck()
+        pc.load_protein_from_pdb(str(protein_file))
+        # Adjust the process_row function to work with dictionaries
+        global process_row
+
+        def process_row(row):
+            mol = [row["Molecule"]]
+            pc.load_ligands_from_mols(mol)
+            clashes = pc.calculate_clashes()
+            strain = pc.calculate_strain_energy()
+            row["clashes"] = clashes[0]
+            row["strain"] = round(strain[0], 3)
+            return row
+
+        # Convert DataFrame to a list of dicts, which is compatible with multiprocessing
+        rows = dataframe.to_dict("records")
+
+        # Create a pool of worker processes
+        pool = multiprocessing.Pool(n_cpus)
+
+        # Use the pool to process rows in parallel
+        processed_rows = pool.map(process_row, rows)
+
+        # Close the pool and wait for the work to finish
+        pool.close()
+        pool.join()
+
+        # Convert list of dicts back to DataFrame
+        dataframe = pd.DataFrame(processed_rows)
+
+        # Filter the dataframe based on clash and strain cutoffs
+        dataframe = dataframe[dataframe["clashes"] <= clash_cutoff]
+        dataframe = dataframe[dataframe["strain"] <= strain_cutoff]
+
+        toc = time.perf_counter()
+        printlog(f"Pose checking completed in {toc - tic:.2f} seconds")
+    except Exception as e:
+        printlog("ERROR: Failed to check poses with PoseCheck!")
+        printlog(str(e))
+
+    return dataframe.drop(columns=["clashes", "strain"])
