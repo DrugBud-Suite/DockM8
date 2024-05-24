7a397f6ff5afcc98257f08b6a1b2bd1b83a10ab4
Tonylac77 <alacournola@gmail.com>
2024-05-25 00:27:35 +0200

    add pose minimization test

--- tests/docking_postprocessing/test_docking_postprocessing.py
+++ tests/docking_postprocessing/test_docking_postprocessing.py
@@ -6,9 +6,7 @@ from pathlib import Path
 from rdkit.Chem import PandasTools
 
 # Search for 'DockM8' in parent directories
-tests_path = next((p / "tests"
-                   for p in Path(__file__).resolve().parents
-                   if (p / "tests").is_dir()), None)
+tests_path = next((p / "tests" for p in Path(__file__).resolve().parents if (p / "tests").is_dir()), None)
 dockm8_path = tests_path.parent
 sys.path.append(str(dockm8_path))
 
@@ -17,43 +15,34 @@ from scripts.docking_postprocessing.docking_postprocessing import docking_postpr
 
 @pytest.fixture
 def common_test_data():
-    """Set up common test data."""
-    dockm8_path = next((p / "tests"
-                        for p in Path(__file__).resolve().parents
-                        if (p / "tests").is_dir()), None).parent
-    input_sdf = Path(
-        dockm8_path /
-        "tests/test_files/docking_postprocessing/example_poses_1fvv.sdf")
-    output_path = Path(
-        dockm8_path /
-        "tests/test_files/docking_postprocessing/example_poses_1fvv_postprocessed.sdf"
-    )
-    protein_file = Path(
-        dockm8_path /
-        "tests/test_files/docking_postprocessing/example_prepared_receptor_1fvv.pdb"
-    )
-    bust_poses = True
-    strain_cutoff = 5
-    clash_cutoff = 3
-    n_cpus = int(os.cpu_count() * 0.9)
-    return input_sdf, output_path, protein_file, bust_poses, strain_cutoff, clash_cutoff, n_cpus
+	"""Set up common test data."""
+	dockm8_path = next((p / "tests" for p in Path(__file__).resolve().parents if (p / "tests").is_dir()), None).parent
+	input_sdf = Path(dockm8_path / "tests/test_files/docking_postprocessing/example_poses_1fvv.sdf")
+	output_path = Path(dockm8_path / "tests/test_files/docking_postprocessing/example_poses_1fvv_postprocessed.sdf")
+	protein_file = Path(dockm8_path / "tests/test_files/docking_postprocessing/example_prepared_receptor_1fvv.pdb")
+	bust_poses = True
+	minimize_poses = True
+	strain_cutoff = 5
+	clash_cutoff = 3
+	n_cpus = int(os.cpu_count() * 0.9)
+	return input_sdf, output_path, protein_file, minimize_poses, bust_poses, strain_cutoff, clash_cutoff, n_cpus
 
 
 @pytest.fixture
 def cleanup(request):
-    """Cleanup fixture to remove generated files after each test."""
-    output_dir = dockm8_path / "tests/test_files/docking_postprocessing/"
+	"""Cleanup fixture to remove generated files after each test."""
+	output_dir = dockm8_path / "tests/test_files/docking_postprocessing/"
 
-    def remove_created_files():
-        for file in output_dir.iterdir():
-            if file.name in ["example_poses_1fvv_postprocessed.sdf"]:
-                file.unlink()
+	def remove_created_files():
+		for file in output_dir.iterdir():
+			if file.name in ["example_poses_1fvv_postprocessed.sdf"]:
+				file.unlink()
 
-    request.addfinalizer(remove_created_files)
+	request.addfinalizer(remove_created_files)
 
 
 def test_docking_postprocessing(common_test_data, cleanup):
-    """
+	"""
     Test case for docking_postprocessing function.
 
     Args:
@@ -62,18 +51,23 @@ def test_docking_postprocessing(common_test_data, cleanup):
     Returns:
         None
     """
-    input_sdf, output_path, protein_file, bust_poses, strain_cutoff, clash_cutoff, n_cpus = common_test_data
-    result = docking_postprocessing(input_sdf, output_path, protein_file,
-                                    bust_poses, strain_cutoff, clash_cutoff,
-                                    n_cpus)
-    assert result == output_path
-    assert output_path.exists()
-    output_data = PandasTools.LoadSDF(str(output_path))
-    assert len(output_data) == 14
+	input_sdf, output_path, protein_file, minimize_poses, bust_poses, strain_cutoff, clash_cutoff, n_cpus = common_test_data
+	result = docking_postprocessing(input_sdf,
+									output_path,
+									protein_file,
+									minimize_poses,
+									bust_poses,
+									strain_cutoff,
+									clash_cutoff,
+									n_cpus)
+	assert result == output_path
+	assert output_path.exists()
+	output_data = PandasTools.LoadSDF(str(output_path))
+	assert len(output_data) == 14
 
 
 def test_docking_postprocessing_without_pose_busting(common_test_data, cleanup):
-    """
+	"""
     Test case for docking_postprocessing function without pose busting.
 
     Args:
@@ -82,17 +76,25 @@ def test_docking_postprocessing_without_pose_busting(common_test_data, cleanup):
     Returns:
         None
     """
-    input_sdf, output_path, protein_file, bust_poses, strain_cutoff, clash_cutoff, n_cpus = common_test_data
-    bust_poses = False
-    result = docking_postprocessing(input_sdf, output_path, protein_file,
-                                    bust_poses, strain_cutoff, clash_cutoff,
-                                    n_cpus)
-    assert result == output_path
-    assert output_path.exists()
+	input_sdf, output_path, protein_file, minimize_poses, bust_poses, strain_cutoff, clash_cutoff, n_cpus = common_test_data
+	bust_poses = False
+	minimize_poses = False
+	result = docking_postprocessing(input_sdf,
+									output_path,
+									protein_file,
+									minimize_poses,
+									bust_poses,
+									strain_cutoff,
+									clash_cutoff,
+									n_cpus)
+	assert result == output_path
+	assert output_path.exists()
+	output_data = PandasTools.LoadSDF(str(output_path))
+	assert len(output_data) == 14
 
 
 def test_docking_postprocessing_with_no_cutoffs(common_test_data, cleanup):
-    """
+	"""
     Test case for docking_postprocessing function with no strain or clash cutoffs.
 
     Args:
@@ -101,11 +103,46 @@ def test_docking_postprocessing_with_no_cutoffs(common_test_data, cleanup):
     Returns:
         None
     """
-    input_sdf, output_path, protein_file, bust_poses, strain_cutoff, clash_cutoff, n_cpus = common_test_data
-    strain_cutoff = None
-    clash_cutoff = None
-    result = docking_postprocessing(input_sdf, output_path, protein_file,
-                                    bust_poses, strain_cutoff, clash_cutoff,
-                                    n_cpus)
-    assert result == output_path
-    assert output_path.exists()
+	input_sdf, output_path, protein_file, minimize_poses, bust_poses, strain_cutoff, clash_cutoff, n_cpus = common_test_data
+	strain_cutoff = None
+	clash_cutoff = None
+	result = docking_postprocessing(input_sdf,
+									output_path,
+									protein_file,
+									minimize_poses,
+									bust_poses,
+									strain_cutoff,
+									clash_cutoff,
+									n_cpus)
+	assert result == output_path
+	assert output_path.exists()
+	output_data = PandasTools.LoadSDF(str(output_path))
+	assert len(output_data) == 40
+
+
+def test_docking_postprocessing_minimization(common_test_data, cleanup):
+	"""
+    Test case for docking_postprocessing function with minimization.
+
+    Args:
+        common_test_data: A tuple containing the input_sdf, output_path, protein_file, bust_poses, strain_cutoff, clash_cutoff, and n_cpus.
+
+    Returns:
+        None
+    """
+	input_sdf, output_path, protein_file, bust_poses, minimize_poses, strain_cutoff, clash_cutoff, n_cpus = common_test_data
+	bust_poses = False
+	strain_cutoff = None
+	clash_cutoff = None
+	result = docking_postprocessing(input_sdf,
+									output_path,
+									protein_file,
+									minimize_poses,
+									bust_poses,
+									strain_cutoff,
+									clash_cutoff,
+									n_cpus)
+	assert result == output_path
+	assert output_path.exists()
+	output_data = PandasTools.LoadSDF(str(output_path))
+	assert len(output_data) == 40
