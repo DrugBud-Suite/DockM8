b905fb640681b6c16ec9a7c4644bc5e9b137e0cd
Tonylac77 <alacournola@gmail.com>
2024-05-25 15:45:58 +0200

    add classy pose processing

--- scripts/docking_postprocessing/docking_postprocessing.py
+++ scripts/docking_postprocessing/docking_postprocessing.py
@@ -19,6 +19,11 @@ from scripts.utilities.utilities import parallel_SDF_loader, printlog
 warnings.filterwarnings("ignore")
 
 
+def log_dataframe_length_change(previous_length, current_length, process_name):
+	if previous_length != current_length:
+		printlog(f"Removed {previous_length - current_length} poses during {process_name} postprocessing.")
+
+
 def docking_postprocessing(input_sdf: Path,
 							output_path: Path,
 							protein_file: Path,
@@ -26,8 +31,10 @@ def docking_postprocessing(input_sdf: Path,
 							bust_poses: bool,
 							strain_cutoff: int,
 							clash_cutoff: int,
+							classy_pose: bool,
+							classy_pose_model: str,
 							n_cpus: int,
-							):
+							) -> Path:
 	"""
     Perform postprocessing on docking results.
 
@@ -35,9 +42,12 @@ def docking_postprocessing(input_sdf: Path,
         input_sdf (Path): Path to the input SDF file containing docking results.
         output_path (Path): Path to save the postprocessed SDF file.
         protein_file (Path): Path to the protein file used for docking.
+        minimize_poses (bool): Flag indicating whether to perform ligand minimization.
         bust_poses (bool): Flag indicating whether to apply pose busting.
         strain_cutoff (int): Cutoff value for strain energy. Poses with strain energy above this cutoff will be removed.
         clash_cutoff (int): Cutoff value for clash score. Poses with clash score above this cutoff will be removed.
+        classy_pose (bool): Flag indicating whether to perform classy pose analysis.
+        classy_pose_model (str): Path to the classy pose model file.
         n_cpus (int): Number of CPUs to use for parallel processing.
 
     Returns:
@@ -45,32 +55,30 @@ def docking_postprocessing(input_sdf: Path,
     """
 	printlog("Postprocessing docking results...")
 	sdf_dataframe = parallel_SDF_loader(input_sdf, molColName="Molecule", idName="Pose ID", n_cpus=n_cpus)
-	starting_length = len(sdf_dataframe)
+	initial_length = len(sdf_dataframe)
+
 	if minimize_poses:
-		minimized_ligands = minimize_all_ligands(protein_file,
-													str(input_sdf),
-													Path(str(input_sdf).replace('.sdf', '_minimized.sdf')),
-													n_cpus)
-	else:
-		step1_length = starting_length
+		sdf_dataframe = minimize_all_ligands(protein_file, str(input_sdf), n_cpus)
+	log_dataframe_length_change(initial_length, len(sdf_dataframe), "Ligand Minimization")
+	initial_length = len(sdf_dataframe)
+
 	if (strain_cutoff is not None) and (clash_cutoff is not None):
-		checked_poses = pose_checker(sdf_dataframe, protein_file, clash_cutoff, strain_cutoff, n_cpus)
-		sdf_dataframe = checked_poses
-		step1_length = len(sdf_dataframe)
-		printlog(f"Removed {starting_length - step1_length} poses during PoseChecker postprocessing.")
-	else:
-		step1_length = starting_length
+		sdf_dataframe = pose_checker(sdf_dataframe, protein_file, clash_cutoff, strain_cutoff, n_cpus)
+		log_dataframe_length_change(initial_length, len(sdf_dataframe), "PoseChecker")
+		initial_length = len(sdf_dataframe)
+
 	if bust_poses:
 		sdf_dataframe = pose_buster(sdf_dataframe, protein_file, n_cpus)
-		step2_length = len(sdf_dataframe)
-		if step1_length:
-			printlog(f"Removed {step1_length - step2_length} poses during PoseBusters postprocessing.")
-		else:
-			printlog(f"Removed {starting_length - step2_length} poses during PoseBusters postprocessing.")
-	os.unlink(minimized_ligands) if minimize_poses else None
+		log_dataframe_length_change(initial_length, len(sdf_dataframe), "PoseBusters")
+		initial_length = len(sdf_dataframe)
+
+	if classy_pose:
+		sdf_dataframe = classy_pose(sdf_dataframe, protein_file, classy_pose_model, n_cpus)
+		log_dataframe_length_change(initial_length, len(sdf_dataframe), "ClassyPose")
+
 	PandasTools.WriteSDF(sdf_dataframe,
-			str(output_path),
-			molColName="Molecule",
-			idName="Pose ID",
-			properties=list(sdf_dataframe.columns))
+							str(output_path),
+							molColName="Molecule",
+							idName="Pose ID",
+							properties=list(sdf_dataframe.columns))
 	return output_path
