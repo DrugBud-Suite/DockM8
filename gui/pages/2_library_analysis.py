4cd464c2e42fb7e6731687a7235f8448a4511b5b
Tonylac77 <alacournola@gmail.com>
2024-06-05 12:48:17 +0200

    add chemical space plotting

--- gui/pages/2_library_analysis.py
+++ gui/pages/2_library_analysis.py
@@ -4,6 +4,10 @@ from pathlib import Path
 import streamlit as st
 from rdkit.Chem import PandasTools
 import plotly.express as px
+from rdkit.Chem import AllChem
+from umap import UMAP
+import pandas as pd
+import numpy as np
 
 # Search for 'DockM8' in parent directories
 gui_path = next((p / "gui" for p in Path(__file__).resolve().parents if (p / "gui").is_dir()), None)
@@ -14,6 +18,8 @@ st.set_page_config(page_title="DockM8", page_icon="./media/DockM8_logo.png", lay
 
 from gui.menu import menu
 from scripts.library_analysis.descriptor_calculation import calculate_properties
+from gui.utils import filter_dataframe
+from scripts.library_analysis.chemical_space import visualize_chemical_space
 
 menu()
 
@@ -26,60 +32,56 @@ def plot_histograms(df, properties):
 			st.plotly_chart(fig)
 
 
-def app():
-	st.title("Library Analysis and Filtering")
+st.title("Library Analysis and Filtering")
 
-	# Search for 'DockM8' in parent directories
-	gui_path = next((p / "gui" for p in Path(__file__).resolve().parents if (p / "gui").is_dir()), None)
-	dockm8_path = gui_path.parent
-	sys.path.append(str(dockm8_path))
+# Search for 'DockM8' in parent directories
+gui_path = next((p / "gui" for p in Path(__file__).resolve().parents if (p / "gui").is_dir()), None)
+dockm8_path = gui_path.parent
+sys.path.append(str(dockm8_path))
 
-	docking_library = st.text_input(label="Entre the path to the ligand library file (.sdf format)",
-									value=str(dockm8_path / "tests" / "test_files" / "library.sdf"),
-									help="Choose a ligand library file (.sdf format)")
-	
-	st.subheader("Select the properties you want to calculate:")
-	col1, col2, col3, col4, col5 = st.columns(5)
-	properties = {
-		'MW': col1.checkbox('Molecular Weight (MW)', value=True),
-		'TPSA': col2.checkbox('Polar Surface Area (TPSA)', value=True),
-		'LogD': col3.checkbox('LogD', value=True),
-		'HBA': col4.checkbox('Hydrogen Bond Acceptors (HBA)', value=True),
-		'HBD': col5.checkbox('Hydrogen Bond Donors (HBD)', value=True),
-		'Rotatable Bonds': col1.checkbox('Number of Rotatable Bonds', value=True),
-		'QED': col2.checkbox('Quantitative Estimate of Drug-likeness (QED)', value=True),
-		'sp3 percentage': col3.checkbox('sp3 Carbon Percentage', value=True),
-		'Ring Count': col4.checkbox('Ring Count', value=True)}
+docking_library = st.text_input(label="Entre the path to the ligand library file (.sdf format)",
+								value=str(dockm8_path / "tests" / "test_files" / "library.sdf"),
+								help="Choose a ligand library file (.sdf format)")
 
-	if st.button("Load Library and Calculate Properties"):
-		if Path(docking_library).is_file():
-			ligand_library = PandasTools.LoadSDF(docking_library,
-													smilesName="SMILES",
-													molColName="Molecule",
-													idName="ID")
-			st.success("Library loaded and properties are being calculated...")
-			ligand_library = calculate_properties(ligand_library, properties)
-			st.dataframe(ligand_library.head())
+st.subheader("Select the properties you want to calculate:")
+col1, col2, col3, col4 = st.columns(4)
+properties = {
+	'MW': col1.checkbox('Molecular Weight (MW)', value=True),
+	'TPSA': col2.checkbox('Polar Surface Area (TPSA)', value=True),
+	'HBA': col3.checkbox('H-Bond Acceptors (HBA)', value=True),
+	'HBD': col4.checkbox('H-Bond Donors (HBD)', value=True),
+	'Rotatable Bonds': col1.checkbox('Number of Rotatable Bonds', value=True),
+	'QED': col2.checkbox('Estimate of Drug-likeness (QED)', value=True),
+	'sp3 percentage': col3.checkbox('sp3 Percentage', value=True),
+	'Ring Count': col4.checkbox('Ring Count', value=True)}
 
-			# Allow filtering on histograms
-			st.subheader("Adjust the ranges for dynamic filtering:")
-			filters = {}
-			for prop in properties:
-				if properties[prop]:
-					col1, col2 = st.columns(2)
-					with col1:
-						min_val = float(ligand_library[prop].min())
-						max_val = float(ligand_library[prop].max())
-						filters[prop] = st.slider(f'Filter {prop}',
-													min_val,
-													max_val, (min_val, max_val),
-													key=f"{prop}_filter")
-					ligand_library = ligand_library[(ligand_library[prop] >= filters[prop][0]) &
-													(ligand_library[prop] <= filters[prop][1])]
+if 'ligand_library' not in st.session_state or st.button("Load Library and Calculate Properties"):
+    if Path(docking_library).is_file():
+        ligand_library = PandasTools.LoadSDF(docking_library,
+                                             smilesName="SMILES",
+                                             molColName="Molecule",
+                                             idName="ID")
+        st.write(f"Ligand library loaded with {len(ligand_library)} compounds.")
+        st.session_state['ligand_library'] = calculate_properties(ligand_library, properties)
+    else:
+        st.error("File does not exist.")
 
-			plot_histograms(ligand_library, properties)
-		else:
-			st.error(f"Invalid file path: {docking_library}")
+# Use session state data for filtering
+if 'ligand_library' in st.session_state:
+    filtered_df = filter_dataframe(st.session_state['ligand_library'])
+    
+    st.write(f"Filtered library contains {len(filtered_df)} compounds. ")
+    st.dataframe(filtered_df)
 
+# Streamlit UI components to capture user input
+method = st.selectbox('Select Dimensionality Reduction Method:', ['UMAP', 'TSNE', 'PCA'])
+fingerprint = st.selectbox('Select Fingerprint Type:', ['ECFP4', 'FCFP4', 'MACCS', 'Torsion'])
 
-app()
+# Visualize chemical space
+if st.button("Visualise Chemical Space"):
+	if 'ligand_library' in st.session_state:
+		st.write("Calculating fingerprints and performing dimensionality reduction...")
+		fig = visualize_chemical_space(st.session_state['ligand_library'], method, fingerprint)
+		st.plotly_chart(fig)
+	else:
+		st.write("Please load and calculate the library first.")
