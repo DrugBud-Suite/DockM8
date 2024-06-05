21d2d2b06a63b324dcad97d7f36779aff16f46d0
Tonylac77 <alacournola@gmail.com>
2024-06-05 12:49:25 +0200

    remove logd from descriptors

--- scripts/library_analysis/descriptor_calculation.py
+++ scripts/library_analysis/descriptor_calculation.py
@@ -4,20 +4,26 @@ from rdkit.Chem import Descriptors, rdMolDescriptors, QED
 def calculate_properties(df, properties):
 	if 'MW' in properties:
 		df['MW'] = df['Molecule'].apply(lambda x: Descriptors.MolWt(x))
+		df['MW'] = df['MW'].astype(float)
 	if 'TPSA' in properties:
 		df['TPSA'] = df['Molecule'].apply(lambda x: Descriptors.TPSA(x))
-	if 'LogD' in properties:
-		df['LogD'] = df['Molecule'].apply(lambda x: rdMolDescriptors.CalcCrippenDescriptors(x)[1])
+		df['TPSA'] = df['TPSA'].astype(float)
 	if 'HBA' in properties:
 		df['HBA'] = df['Molecule'].apply(lambda x: rdMolDescriptors.CalcNumHBA(x))
+		df['HBA'] = df['HBA'].astype(int)
 	if 'HBD' in properties:
 		df['HBD'] = df['Molecule'].apply(lambda x: rdMolDescriptors.CalcNumHBD(x))
+		df['HBD'] = df['HBD'].astype(int)
 	if 'Rotatable Bonds' in properties:
 		df['Rotatable Bonds'] = df['Molecule'].apply(lambda x: Descriptors.NumRotatableBonds(x))
+		df['Rotatable Bonds'] = df['Rotatable Bonds'].astype(int)
 	if 'QED' in properties:
 		df['QED'] = df['Molecule'].apply(lambda x: QED.qed(x))
+		df['QED'] = df['QED'].astype(float)
 	if 'sp3 percentage' in properties:
 		df['sp3 percentage'] = df['Molecule'].apply(lambda x: rdMolDescriptors.CalcFractionCSP3(x))
+		df['sp3 percentage'] = df['sp3 percentage'].astype(float)
 	if 'Ring Count' in properties:
 		df['Ring Count'] = df['Molecule'].apply(lambda x: rdMolDescriptors.CalcNumRings(x))
+		df['Ring Count'] = df['Ring Count'].astype(int)
 	return df
