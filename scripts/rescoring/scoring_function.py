244a0b715bf5597b3e5c666ecf937e0ce6f9e06e
Tonylac77 <alacournola@gmail.com>
2024-07-07 15:57:05 +0200

    create ScoringFunction class

---
+++ scripts/rescoring/scoring_function.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+import pandas as pd
+from pathlib import Path
+from typing import Dict, Any
+
+
+class ScoringFunction(ABC):
+
+	def __init__(self, name: str, column_name: str, best_value: str, score_range: tuple):
+		self.name = name
+		self.column_name = column_name
+		self.best_value = best_value
+		self.score_range = score_range
+
+	@abstractmethod
+	def rescore(self, sdf: str, n_cpus: int, **kwargs) -> pd.DataFrame:
+		pass
+
+	def get_info(self) -> Dict[str, Any]:
+		return {
+			"name": self.name,
+			"column_name": self.column_name,
+			"best_value": self.best_value,
+			"range": self.score_range}
