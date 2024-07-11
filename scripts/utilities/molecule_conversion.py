4af0de128ccded8b0540f636b0c903354dce40a0
Tonylac77 <alacournola@gmail.com>
2024-07-11 22:17:03 +0200

    add docs

--- scripts/utilities/molecule_conversion.py
+++ scripts/utilities/molecule_conversion.py
@@ -1,13 +1,12 @@
 import os
-from pathlib import Path
+import subprocess
 import sys
 import warnings
-import subprocess
-from meeko import MoleculePreparation
-from meeko import PDBQTWriterLegacy
-import openbabel
-from openbabel import pybel
+from pathlib import Path
 from rdkit import Chem
+from meeko import MoleculePreparation, PDBQTWriterLegacy
+
+from openbabel import pybel
 
 # Search for 'DockM8' in parent directories
 scripts_path = next((p / "scripts" for p in Path(__file__).resolve().parents if (p / "scripts").is_dir()), None)
@@ -22,106 +21,166 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 pybel.ob.obErrorLog.StopLogging()
 
 
-def convert_molecules(input_file: Path, output_file: Path, input_format: str, output_format: str):
+def find_mgltools_installation(software_path):
 	"""
-    Convert molecules from one file format to another.
+	Find the MGLTools installation directory.
 
-    Args:
-        input_file (Path): The path to the input file.
-        output_file (Path): The path to the output file.
-        input_format (str): The format of the input file.
-        output_format (str): The format of the output file.
+	Parameters:
+	- software_path (str): The path to the software installation directory.
 
 	Returns:
-        Path: The path to the converted output file.
-    """
-	# For protein conversion to pdbqt file format using OpenBabel
+	- str: The path to the MGLTools installation directory.
+
+	Raises:
+	- FileNotFoundError: If the MGL_Tools directory or MGLToolsPckgs directory is not found.
+	"""
+	mgl_tools_dir = software_path / 'MGL_Tools'
+	if not mgl_tools_dir.exists():
+		raise FileNotFoundError(f"MGL_Tools directory not found at {mgl_tools_dir}")
+
+	# Look for MGLToolsPckgs directory
+	mgltools_pckgs = list(mgl_tools_dir.glob('**/MGLToolsPckgs'))
+	if not mgltools_pckgs:
+		raise FileNotFoundError(f"MGLToolsPckgs directory not found in {mgl_tools_dir}")
+
+	return mgltools_pckgs[0].parent
+
+
+def get_mgltools_env(software_path):
+	"""
+	Get environment variables for MGLTools.
+
+	Args:
+		software_path (str): The path to the MGLTools software installation.
+
+	Returns:
+		dict: A dictionary containing the environment variables for MGLTools.
+
+	Raises:
+		FileNotFoundError: If the required directories or files are not found.
+		RuntimeError: If the pythonsh executable is not runnable.
+
+	"""
+	mgltools_path = find_mgltools_installation(software_path)
+
+	bin_dir = mgltools_path / 'bin'
+	if not bin_dir.exists():
+		raise FileNotFoundError(f"bin directory not found at {bin_dir}")
+
+	pythonsh = bin_dir / 'pythonsh'
+	if not pythonsh.exists():
+		raise FileNotFoundError(f"pythonsh not found at {pythonsh}")
+
+	utilities_dir = mgltools_path / 'MGLToolsPckgs' / 'AutoDockTools' / 'Utilities24'
+	if not utilities_dir.exists():
+		raise FileNotFoundError(f"Utilities24 directory not found at {utilities_dir}")
+
+	env = os.environ.copy()
+	env['PATH'] = f"{bin_dir}:{env['PATH']}"
+	env['MGL'] = str(mgltools_path)
+	env['MGLPY'] = str(pythonsh)
+	env['MGLUTIL'] = str(utilities_dir)
+
+	# Verify that pythonsh is runnable
+	try:
+		subprocess.run([pythonsh, '--version'], check=True, capture_output=True)
+	except subprocess.CalledProcessError:
+		raise RuntimeError(f"pythonsh at {pythonsh} is not runnable")
+
+	return env
+
+
+def convert_molecules(input_file: Path,
+						output_file_or_path: Path,
+						input_format: str,
+						output_format: str,
+						software: Path):
+	"""
+	Converts molecules from one format to another using various conversion tools.
+
+	Args:
+		input_file (Path): The path to the input file containing the molecules.
+		output_file_or_path (Path): The path to the output file or directory where the converted molecules will be saved.
+		input_format (str): The format of the input molecules.
+		output_format (str): The desired format of the output molecules.
+		software (Path): The path to the software used for conversion.
+
+	Returns:
+		The path(s) to the converted molecule file(s).
+
+	Raises:
+		FileNotFoundError: If the input file for molecule conversion is not found.
+		subprocess.CalledProcessError: If an error occurs during conversion using MGLTools.
+		Exception: If an error occurs during conversion using Meeko or Pybel.
+	"""
+	try:
+		mgl_env = get_mgltools_env(software)
+	except (FileNotFoundError, RuntimeError) as e:
+		printlog(f"Error setting up MGLTools environment: {str(e)}")
+		raise
+
+	if not input_file.exists():
+		raise FileNotFoundError(f"Input file for molecule conversion not found: {input_file}")
+
+	# For protein conversion to pdbqt file format using MGLTools
 	if input_format == "pdb" and output_format == "pdbqt":
 		try:
-			mol = Chem.MolFromPDBFile(str(input_file), sanitize=False, removeHs=False)
-			lines = [x.strip() for x in open(input_file).readlines()]
-			out_lines = []
-			for line in lines:
-				if "ROOT" in line or "ENDROOT" in line or "TORSDOF" in line:
-					out_lines.append("%s\n" % line)
-					continue
-				if not line.startswith("ATOM"):
-					continue
-				line = line[:66]
-				atom_index = int(line[6:11])
-				atom = mol.GetAtoms()[atom_index - 1]
-				line = "%s    +0.000 %s\n" % (line, atom.GetSymbol().ljust(2))
-				out_lines.append(line)
-			with open(output_file, 'w') as fout:
-				for line in out_lines:
-					fout.write(line)
-			return output_file
-		except Exception as e:
-			printlog(f"Error occurred during conversion using RDkit: {str(e)}. Trying with Meeko...")
-		try:
-			# Run the pdb2pqr command
-			subprocess.call(
-				f"pdb2pqr --ff=AMBER --ffout=AMBER --keep-chain --nodebump {input_file} {input_file.with_suffix('.pqr')} -q",
-				shell=True,
-				stdout=subprocess.DEVNULL,
-				stderr=subprocess.DEVNULL)
-			# Run the mk_prepare_receptor.py script
-			subprocess.call(
-				f"{dockm8_path}/scripts/utilities/mk_prepare_receptor.py --pdb {input_file.with_suffix('.pqr')} -o {output_file} --skip_gpf",
-				shell=True,
-				stdout=subprocess.DEVNULL,
-				stderr=subprocess.DEVNULL)
-			os.remove(input_file.with_suffix(".pqr")) if os.path.exists(input_file.with_suffix(".pqr")) else None
-			return output_file
-		except Exception as e:
-			printlog(f"Error occurred during conversion using PDB2PQR and Meeko: {str(e)}. Trying with OpenBabel...")
-			try:
-				obConversion = openbabel.OBConversion()
-				mol = openbabel.OBMol()
-				obConversion.ReadFile(mol, str(input_file))
-				obConversion.SetInAndOutFormats("pdb", "pdbqt")
-				# Calculate Gasteiger charges
-				charge_model = openbabel.OBChargeModel.FindType("gasteiger")
-				charge_model.ComputeCharges(mol)
-				obConversion.WriteFile(mol, str(output_file))
-				# Remove all torsions from pdbqt output
-				with open(output_file, "r") as file:
-					lines = file.readlines()
-					lines = [
-						line for line in lines if all(keyword not in line for keyword in [
-							"between atoms:", "BRANCH", "ENDBRANCH", "torsions", "Active", "ENDROOT", "ROOT", ])]
-					lines = [line.replace(line, "TER\n") if line.startswith("TORSDOF") else line for line in lines]
-					with open(output_file, "w") as file:
-						file.writelines(lines)
-			except Exception as e:
-				printlog(f"Error occurred during conversion using OpenBabel: {str(e)}")
-			return output_file
-	# For compound conversion to pdbqt file format using RDKit and Meeko
+			prepare_receptor_script = Path(mgl_env['MGLUTIL']) / "prepare_receptor4.py"
+			cmd = f"{mgl_env['MGLPY']} {prepare_receptor_script} -r {input_file} -o {output_file_or_path} -A checkhydrogens"
+			subprocess.run(cmd, shell=True, check=True, env=mgl_env)
+			return output_file_or_path
+		except subprocess.CalledProcessError as e:
+			printlog(f"Error occurred during conversion using MGLTools prepare_receptor4.py: {str(e)}")
+			raise
+
+	# For compound conversion to pdbqt file format using MGLTools
 	if input_format == "sdf" and output_format == "pdbqt":
 		try:
 			pdbqt_files = []
-			for mol in Chem.SDMolSupplier(str(input_file), removeHs=False):
+			mols = list(Chem.SDMolSupplier(str(input_file), removeHs=False))
+
+			if len(mols) == 1 and output_file_or_path.suffix == '.pdbqt':
+				# Single compound, output is a file
+				mol = mols[0]
 				preparator = MoleculePreparation(min_ring_size=10)
 				mol = Chem.AddHs(mol)
 				setup_list = preparator.prepare(mol)
 				pdbqt_string = PDBQTWriterLegacy.write_string(setup_list[0])
-				mol_name = mol.GetProp("_Name")
-				output_path = Path(output_file) / f"{mol_name}.pdbqt"
-				# Write the pdbqt string to the file
-				with open(output_path, "w") as f:
+				with open(output_file_or_path, "w") as f:
 					f.write(pdbqt_string[0])
-				pdbqt_files.append(output_path)
+				pdbqt_files.append(output_file_or_path)
+			else:
+				# Multiple compounds or output is a directory
+				output_dir = output_file_or_path if output_file_or_path.is_dir() else output_file_or_path.parent
+				output_dir.mkdir(parents=True, exist_ok=True)
+
+				for i, mol in enumerate(mols):
+					if mol is None:
+						continue
+					preparator = MoleculePreparation(min_ring_size=10)
+					mol = Chem.AddHs(mol)
+					setup_list = preparator.prepare(mol)
+					pdbqt_string = PDBQTWriterLegacy.write_string(setup_list[0])
+					mol_name = mol.GetProp("_Name") if mol.HasProp("_Name") else f"compound_{i+1}"
+					output_path = output_dir / f"{mol_name}.pdbqt"
+					with open(output_path, "w") as f:
+						f.write(pdbqt_string[0])
+					pdbqt_files.append(output_path)
+
+			return pdbqt_files
 		except Exception as e:
 			printlog(f"Error occurred during conversion using Meeko: {str(e)}")
-		return pdbqt_files
+			raise
+
 	# For general conversion using Pybel
 	else:
 		pybel.ob.obErrorLog.StopLogging()
 		try:
-			output = pybel.Outputfile(output_format, str(output_file), overwrite=True)
+			output = pybel.Outputfile(output_format, str(output_file_or_path), overwrite=True)
 			for mol in pybel.readfile(input_format, str(input_file)):
 				output.write(mol)
 			output.close()
+			return output_file_or_path
 		except Exception as e:
 			printlog(f"Error occurred during conversion using Pybel: {str(e)}")
-		return output_file
+			raise
