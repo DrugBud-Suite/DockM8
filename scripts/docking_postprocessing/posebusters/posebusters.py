a0a51509458e23d061ce34e03f018bc83455658e
Tonylac77 <alacournola@gmail.com>
2024-07-14 10:44:56 +0200

    refactor to use parallel executor

--- scripts/docking_postprocessing/posebusters/posebusters.py
+++ scripts/docking_postprocessing/posebusters/posebusters.py
@@ -1,10 +1,8 @@
 import os
 import sys
 import time
-from concurrent.futures import ProcessPoolExecutor
 from pathlib import Path
 
-import numpy as np
 import pandas as pd
 from yaml import safe_load
 
@@ -14,6 +12,7 @@ dockm8_path = scripts_path.parent
 sys.path.append(str(dockm8_path))
 
 from scripts.utilities.logging import printlog
+from scripts.utilities.parallel_executor import parallel_executor
 
 
 def process_chunk(data_chunk, config_path, protein_file):
@@ -21,12 +20,11 @@ def process_chunk(data_chunk, config_path, protein_file):
 
 	# Initialize PoseBusters with the configuration file
 	buster = PoseBusters(config=safe_load(open(config_path)))
-	data_chunk["mol_cond"] = str(protein_file)                          # Adding a new column based on protein_file
-	data_chunk = data_chunk.rename(columns={"Molecule": "mol_pred"})  # Renaming a column
+	data_chunk["mol_cond"] = str(protein_file)
+	data_chunk = data_chunk.rename(columns={"Molecule": "mol_pred"})
 
 	# Apply PoseBusters processing and preserve all existing columns
 	busted_df = buster.bust_table(data_chunk)
-	# Combine the original data_chunk with new columns from df
 	combined_df = pd.concat([data_chunk.reset_index(drop=True), busted_df.reset_index(drop=True)], axis=1)
 	combined_df = combined_df.rename(columns={"mol_pred": "Molecule"})
 	cols_to_check = [
@@ -45,22 +43,36 @@ def process_chunk(data_chunk, config_path, protein_file):
 	return df_final if df_final.shape[0] > 0 else None
 
 
-def pose_buster(dataframe: pd.DataFrame, protein_file: Path, n_cpus: int = os.cpu_count()):
+def pose_buster(dataframe: pd.DataFrame, protein_file: Path, n_cpus: int = (os.cpu_count() * 0.9)):
 	tic = time.perf_counter()
 	printlog("Busting poses...")
 	config_path = str(dockm8_path) + "/scripts/docking_postprocessing/posebusters/posebusters_config.yml"
 
-	# Splitting DataFrame into chunks for parallel processing
-	chunks = np.array_split(dataframe, len(dataframe))
-	results = []
-	with ProcessPoolExecutor(max_workers=n_cpus) as executor:
-		futures = [executor.submit(process_chunk, chunk, config_path, protein_file) for chunk in chunks]
-		for future in futures:
-			results.append(future.result())
-	# Concatenate all processed chunks
-	concatenated_df = pd.concat(results)
-	concatenated_df.reset_index(inplace=True, drop=True)
-	dataframe = dataframe[dataframe["Pose ID"].isin(concatenated_df["Pose ID"])]
+	# Calculate chunk size based on number of CPUs
+	chunk_size = len(dataframe) // n_cpus
+	if chunk_size == 0:
+		chunk_size = 1
+
+	# Split DataFrame into chunks
+	chunks = [dataframe[i:i + chunk_size] for i in range(0, len(dataframe), chunk_size)]
+
+	# Use parallel_executor to process chunks
+	results = parallel_executor(process_chunk,
+								chunks,
+								n_cpus,
+								job_manager="concurrent_process",
+								display_name="PoseBusters",
+								config_path=config_path,
+								protein_file=protein_file)
+
+	# Filter out None results and concatenate valid DataFrames
+	valid_results = [df for df in results if df is not None]
+	if valid_results:
+		concatenated_df = pd.concat(valid_results, ignore_index=True)
+		dataframe = dataframe[dataframe["Pose ID"].isin(concatenated_df["Pose ID"])]
+	else:
+		dataframe = pd.DataFrame()                     # Return an empty DataFrame if no valid results
+
 	toc = time.perf_counter()
 	printlog(f"PoseBusters checking completed in {toc - tic:.2f} seconds.")
 	return dataframe
