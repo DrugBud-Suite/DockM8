b1d0745d3ec50d95053d55604bb5eb75f91e2ef1
Tonylac77 <alacournola@gmail.com>
2024-08-13 10:59:02 +0200

    improve ranking method

--- scripts/consensus/consensus_methods/Pareto_rank_avg.py
+++ scripts/consensus/consensus_methods/Pareto_rank_avg.py
@@ -9,18 +9,30 @@ warnings.filterwarnings("ignore", category=UserWarning)
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
-def is_pareto_efficient(scores: np.ndarray) -> np.ndarray:
+def is_pareto_efficient(costs, return_mask=True):
 	"""
     Find the pareto-efficient points
-    :param scores: An (n_points, n_scores) array
-    :return: A boolean array of pareto-efficient points
+    :param costs: An (n_points, n_costs) array
+    :param return_mask: True to return a mask
+    :return: An array of indices of pareto-efficient points.
+        If return_mask is True, this will be an (n_points, ) boolean array
+        Otherwise it will be a (n_efficient_points, ) integer array of indices.
     """
-	is_efficient = np.ones(scores.shape[0], dtype=bool)
-	for i, c in enumerate(scores):
-		if is_efficient[i]:
-			is_efficient[is_efficient] = np.any(scores[is_efficient] > c, axis=1)
-			is_efficient[i] = True
-	return is_efficient
+	is_efficient = np.arange(costs.shape[0])
+	n_points = costs.shape[0]
+	next_point_index = 0                                                # Next index in the is_efficient array to search for
+	while next_point_index < len(costs):
+		nondominated_point_mask = np.any(costs < costs[next_point_index], axis=1)
+		nondominated_point_mask[next_point_index] = True
+		is_efficient = is_efficient[nondominated_point_mask]               # Remove dominated points
+		costs = costs[nondominated_point_mask]
+		next_point_index = np.sum(nondominated_point_mask[:next_point_index]) + 1
+	if return_mask:
+		is_efficient_mask = np.zeros(n_points, dtype=bool)
+		is_efficient_mask[is_efficient] = True
+		return is_efficient_mask
+	else:
+		return is_efficient
 
 
 def Pareto_rank_avg(df: pd.DataFrame, selected_columns: list, normalize: bool = True) -> pd.DataFrame:
