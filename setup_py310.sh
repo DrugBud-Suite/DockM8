1fd0cb6563f7f6a4e6590604cc05af88b574d669
Tonylac77 <alacournola@gmail.com>
2024-08-14 22:16:03 +0200

    fix reduce install

--- setup_py310.sh
+++ setup_py310.sh
@@ -133,7 +133,7 @@ else
 
     conda install -c conda-forge rdkit=2023.09 spyrmsd kneed molvs xgboost openbabel docopt pdbfixer smina lightning -q -y
 	conda install ipykernel scipy seaborn tqdm pytest pydantic -q -y
-	conda install -c mx reduct -q -y
+	conda install -c mx reduce -q -y
 
     echo -e """
     ###############################################################
