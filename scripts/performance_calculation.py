d9cc168d239bf741102e608a61588000aaa485a7
Tonylac77 <alacournola@gmail.com>
2024-09-09 21:08:40 +0200

    refactor and use DOckM* Results class

--- scripts/performance_calculation.py
+++ scripts/performance_calculation.py
@@ -23,23 +23,30 @@ from scripts.consensus.consensus import CONSENSUS_METHODS
 from scripts.consensus.score_manipulation import rank_scores, standardize_scores
 from scripts.utilities.logging import printlog
 from scripts.utilities.parallel_executor import parallel_executor
+from scripts.results import DockM8Results
 
 warnings.filterwarnings("ignore")
 
 
-def process_combination(combination, docking_program, selection_method, ranked_df, standardised_df, actives_df, percentages):
+def process_combination(combination,
+						selection_method,
+						ranked_df,
+						standardised_df,
+						actives_df,
+						percentages,
+						docking_program='None'):
 	filtered_ranked_df = ranked_df[["ID"] + list(combination)]
 	filtered_standardised_df = standardised_df[["ID"] + list(combination)]
 	combination_dfs = []
 	consensus_dfs = {}
-	
+
 	# For each consensus method
 	for method in CONSENSUS_METHODS.keys():
 		if CONSENSUS_METHODS[method]["type"] == "rank":
 			consensus_dfs[method] = CONSENSUS_METHODS[method]["function"](filtered_ranked_df, list(combination))
 		elif CONSENSUS_METHODS[method]["type"] == "score":
 			consensus_dfs[method] = CONSENSUS_METHODS[method]["function"](filtered_standardised_df, list(combination))
-		
+
 		merged_df = pd.merge(consensus_dfs[method], actives_df, on="ID")
 		# Get the column name that is not 'ID' or 'Activity'
 		col_to_sort = [col for col in merged_df.columns if col not in ["ID", "Activity"]][0]
@@ -47,14 +54,14 @@ def process_combination(combination, docking_program, selection_method, ranked_d
 		merged_df = merged_df.fillna(0)
 		scores = merged_df[col_to_sort].values
 		activities = merged_df["Activity"].values
-		
+
 		# Calculate performance metrics
 		auc_roc = round(roc_auc_score(activities, scores, multi_class="ovo"), 3)
 		bedroc = round(Scoring.CalcBEDROC(list(zip(scores, activities)), 1, 80.5), 3)
 		auc = round(Scoring.CalcAUC(list(zip(scores, activities)), 1), 3)
 		ef_results = [calculate_EF(merged_df, p) for p in percentages]
 		rie = round(Scoring.CalcRIE(list(zip(scores, activities)), 1, 80.5), 3)
-		
+
 		combination_dfs.append(
 			pd.DataFrame(
 				{
@@ -65,109 +72,50 @@ def process_combination(combination, docking_program, selection_method, ranked_d
 					"AUC_ROC": auc_roc,
 					"BEDROC": bedroc,
 					"AUC": auc,
-					**{f"EF_{p}%": ef for p, ef in zip(percentages, ef_results)},
-					"RIE": rie,
-				},
-				index=[0],
-			)
-		)
-	
-	combination_df = pd.concat(combination_dfs, axis=0)
-	return combination_df
-
-
-def calculate_performance_for_clustering_method_OLD(dir, w_dir, actives_df, percentages):
-	clustering_method = ("_".join(dir.split("_")[1:3])
-							if len(dir.split("_")) > 3 else dir.split("_")[1] if len(dir.split("_")) == 3 else None)
-	# Calculate performance for single scoring functions
-	rescored_df = pd.read_csv(Path(w_dir) / dir / "allposes_rescored.csv")
-	standardised_df = standardize_scores(rescored_df, "min_max")
-	standardised_df["ID"] = standardised_df["Pose ID"].str.split("_").str[0]
-	standardised_df["ID"] = standardised_df["ID"].astype(str)
-	score_columns = [col for col in standardised_df.columns if col not in ["Pose ID", "ID"]]
-	result_list = []
-	merged_df = pd.merge(standardised_df, actives_df, on="ID")
-	merged_df.fillna(0, inplace=True)
-	for col in score_columns:
-		merged_df.sort_values(col, ascending=False, inplace=True)
-		scores = merged_df[col].values
-		activities = merged_df["Activity"].values
-		# Calculate EF for all percentages at once using vectorized operations
-		auc_roc = round(roc_auc_score(activities, scores, multi_class="ovo"), 3)
-		bedroc = round(Scoring.CalcBEDROC(list(zip(scores, activities)), 1, 80.5), 3)
-		auc = round(Scoring.CalcAUC(list(zip(scores, activities)), 1), 3)
-		ef_results = [calculate_EF(merged_df, p) for p in percentages]
-		rie = round(Scoring.CalcRIE(list(zip(scores, activities)), 1, 80.5), 3)
-		result_list.append(
-			pd.DataFrame(
-				{
-					"clustering": clustering_method,
-					"consensus": "None",
-					"scoring": col,
-					"AUC_ROC": auc_roc,
-					"BEDROC": bedroc,
-					"AUC": auc,
 					**{
 						f"EF_{p}%": ef for p, ef in zip(percentages, ef_results)},
 					"RIE": rie, },
 				index=[0],
 			))
-	# Calculate performance for consensus scoring functions
-	ranked_df = rank_scores(standardised_df)
-	ranked_df["ID"] = ranked_df["Pose ID"].str.split("_").str[0]
-	ranked_df["ID"] = ranked_df["ID"].astype(str)
-	for length in tqdm(range(2, len(score_columns)), desc=f"{clustering_method}"):
-		combinations = list(itertools.combinations(score_columns, length))
-		# For each combination
-		results = parallel_executor(process_combination,
-									combinations,
-									n_cpus=32,
-									job_manager="concurrent_process_silent",
-									clustering_method=clustering_method,
-									ranked_df=ranked_df,
-									standardised_df=standardised_df,
-									actives_df=actives_df,
-									percentages=percentages)
 
-		for result in results:
-			result_list.append(result)
-	return pd.concat(result_list, axis=0)
+	combination_df = pd.concat(combination_dfs, axis=0)
+	return combination_df
 
 
-def calculate_performance(w_dir: Path, actives_library: Path, percentages: list):
-	"""
-	Calculates the performance of a scoring system for different clustering methods.
-
-	Args:
-		w_dir (Path): The directory path where the scoring results are stored.
-		actives_library (Path): The path to the actives library in SDF format.
-		percentages (list): A list of percentages for calculating the EF (enrichment factor).
-
-	Returns:
-		DataFrame: A DataFrame containing the performance results for different clustering methods, consensus methods, and scoring functions.
-	"""
+def calculate_performance(w_dir: Path, results: DockM8Results, actives_library: Path, percentages: list):
 	printlog("Calculating performance...")
-	# all_results = pd.DataFrame(columns=['clustering', 'consensus', 'scoring'] + [f'EF{p}' for p in percentages])
+
 	# Load actives data
 	actives_df = PandasTools.LoadSDF(str(actives_library), molColName=None, idName="ID")
 	actives_df = actives_df[["ID", "Activity"]]
 	actives_df["Activity"] = pd.to_numeric(actives_df["Activity"])
-	# Calculate performance for each clustering method
-	dirs = [dir for dir in os.listdir(w_dir) if dir.startswith("rescoring") and dir.endswith("clustered")]
-	results = parallel_executor(calculate_performance_for_clustering_method_OLD,
-								dirs,
-								n_cpus=1,
-								job_manager="concurrent_process_silent",
-								w_dir=w_dir,
-								actives_df=actives_df,
-								percentages=percentages)
 
-	all_results = pd.concat(results, ignore_index=True)
-	(w_dir / "performance").mkdir(parents=True, exist_ok=True)
-	all_results.to_csv(Path(w_dir) / "performance" / "performance.csv", index=False)
-	return all_results
+	all_results = []
+
+	for selection_method, rescored_poses in results.rescored_poses.items():
+		# Use join() to combine multiple docking programs if there are more than one
+		docking_programs = ", ".join(results.docking_programs)
+		printlog(f"Processing {docking_programs} with {selection_method} selection method...")
+		rescored_dataframe = pd.read_csv(rescored_poses)
+		performance_results = calculate_performance_for_method(rescored_dataframe,
+																actives_df,
+																percentages,
+																docking_programs,
+																selection_method)
+
+		all_results.append(performance_results)
+
+	combined_results = pd.concat(all_results, ignore_index=True)
+
+	# Save results to CSV
+	output_file = w_dir / "performance.csv"
+	combined_results.to_csv(output_file, index=False)
+	printlog(f"Performance results saved to {output_file}")
+
+	return combined_results
 
 
+# The calculate_EF function remains unchanged
 def calculate_EF(merged_df, percentage: float):
 	total_rows = len(merged_df)
 	N100_percent = total_rows
@@ -183,28 +131,29 @@ def calculate_EF(merged_df, percentage: float):
 	else:
 		return round(ef, 2)
 
+
 def calculate_performance_for_method(rescored_df, actives_df, percentages, docking_program, selection_method):
 	standardised_df = standardize_scores(rescored_df, "min_max")
 	standardised_df["ID"] = standardised_df["Pose ID"].str.split("_").str[0]
 	standardised_df["ID"] = standardised_df["ID"].astype(str)
 	score_columns = [col for col in standardised_df.columns if col not in ["Pose ID", "ID", "Molecule", "SMILES"]]
-	
+
 	result_list = []
 	merged_df = pd.merge(standardised_df, actives_df, on="ID")
 	merged_df.fillna(0, inplace=True)
-	
+
 	# Calculate performance for single scoring functions
 	for col in score_columns:
 		merged_df.sort_values(col, ascending=False, inplace=True)
 		scores = merged_df[col].values
 		activities = merged_df["Activity"].values
-		
+
 		auc_roc = round(roc_auc_score(activities, scores, multi_class="ovo"), 3)
 		bedroc = round(Scoring.CalcBEDROC(list(zip(scores, activities)), 1, 80.5), 3)
 		auc = round(Scoring.CalcAUC(list(zip(scores, activities)), 1), 3)
 		ef_results = [calculate_EF(merged_df, p) for p in percentages]
 		rie = round(Scoring.CalcRIE(list(zip(scores, activities)), 1, 80.5), 3)
-		
+
 		result_list.append(
 			pd.DataFrame(
 				{
@@ -215,94 +164,71 @@ def calculate_performance_for_method(rescored_df, actives_df, percentages, docki
 					"AUC_ROC": auc_roc,
 					"BEDROC": bedroc,
 					"AUC": auc,
-					**{f"EF_{p}%": ef for p, ef in zip(percentages, ef_results)},
-					"RIE": rie,
-				},
+					**{
+						f"EF_{p}%": ef for p, ef in zip(percentages, ef_results)},
+					"RIE": rie, },
 				index=[0],
-			)
-		)
-	
+			))
+
 	# Calculate performance for consensus scoring functions
 	ranked_df = rank_scores(standardised_df)
 	ranked_df["ID"] = ranked_df["Pose ID"].str.split("_").str[0]
 	ranked_df["ID"] = ranked_df["ID"].astype(str)
-	
-	for length in tqdm(range(2, len(score_columns)+1), desc=f"{docking_program}_{selection_method}"):
+
+	for length in tqdm(range(2, len(score_columns) + 1), desc=f"{docking_program}_{selection_method}"):
 		combinations = list(itertools.combinations(score_columns, length))
-		results = parallel_executor(
-			process_combination,
-			combinations,
-			n_cpus=32,
-			job_manager="concurrent_process_silent",
-			docking_program=docking_program,
-			selection_method=selection_method,
-			ranked_df=ranked_df,
-			standardised_df=standardised_df,
-			actives_df=actives_df,
-			percentages=percentages
-		)
-		
+		results = parallel_executor(process_combination,
+									combinations,
+									n_cpus=32,
+									job_manager="concurrent_process_silent",
+									docking_program=docking_program,
+									selection_method=selection_method,
+									ranked_df=ranked_df,
+									standardised_df=standardised_df,
+									actives_df=actives_df,
+									percentages=percentages)
+
 		for result in results:
 			result_list.append(result)
-	
+
 	return pd.concat(result_list, axis=0)
 
-def determine_optimal_conditions(w_dir: Path, results: dict, actives_library: Path, percentages: list):
-	"""
-	Determines the optimal conditions by calculating the performance of scoring systems
-	for different docking programs and selection methods.
 
-	Args:
-		results (dict): A nested dictionary containing rescored poses for each docking program and selection method.
-		actives_library (Path): The path to the actives library in SDF format.
-		percentages (list): A list of percentages for calculating the EF (enrichment factor).
-
-	Returns:
-		DataFrame: A DataFrame containing the performance results for different docking programs,
-				   selection methods, consensus methods, and scoring functions.
-	"""
+def determine_optimal_conditions(w_dir: Path, results: DockM8Results, actives_library: Path, percentages: list):
 	printlog("Determining optimal conditions using the actives library : " + str(actives_library))
-	
+
 	# Load actives data
 	actives_df = PandasTools.LoadSDF(str(actives_library), molColName=None, idName="ID")
 	actives_df = actives_df[["ID", "Activity"]]
 	actives_df["Activity"] = pd.to_numeric(actives_df["Activity"])
-	
+
 	all_results = []
-	
-	for docking_program, selection_methods in results.items():
-		for selection_method, rescored_poses in selection_methods.items():
-			printlog(f"Processing {docking_program} with {selection_method} selection method...")
-			rescored_dataframe = pd.read_csv(rescored_poses)
-			performance_results = calculate_performance_for_method(
-				rescored_dataframe,
-				actives_df,
-				percentages,
-				docking_program,
-				selection_method
-			)
-			
-			all_results.append(performance_results)
-	
+
+	for selection_method, rescored_poses in results.rescored_poses.items():
+		printlog(f"Processing {results.docking_program} with {selection_method} selection method...")
+		rescored_dataframe = pd.read_csv(rescored_poses)
+		performance_results = calculate_performance_for_method(rescored_dataframe,
+																actives_df,
+																percentages,
+																results.docking_program,
+																selection_method)
+
+		all_results.append(performance_results)
+
 	combined_results = pd.concat(all_results, ignore_index=True)
-	
+
 	# Save results to CSV
 	output_file = w_dir / "optimal_conditions.csv"
 	combined_results.to_csv(output_file, index=False)
 	printlog(f"Decoy performance results saved to {output_file}")
-	
+
 	metrics = ['AUC_ROC', 'BEDROC', 'AUC', 'EF_0.5%', 'EF_1%', 'EF_2%', 'EF_5%', 'EF_10%', 'RIE']
 	matrix = combined_results[metrics].values
-	
-	objectives = ["max"] * len(metrics)  # All metrics should be maximized
+
+	objectives = ["max"] * len(metrics)             # All metrics should be maximized
 
 	# Create the decision matrix
-	dm = skc.mkdm(
-		matrix,
-		objectives,
-		alternatives=combined_results.index.tolist(),
-		criteria=metrics,
-	)
+	dm = skc.mkdm(matrix, objectives, alternatives=combined_results.index.tolist(), criteria=metrics, )
 	dec = simple.WeightedSumModel()
 
 	# Get the rankings
